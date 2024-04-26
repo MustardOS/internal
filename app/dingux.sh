2c57efa5a989c40c459897d9e2977585a2306b3f
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-04-26 23:12:40 +1000

    muOS Internals - Reverted to 32-bit library for Dingux and GMU

--- app/dingux.sh
+++ app/dingux.sh
@@ -4,5 +4,6 @@ DINGUX_DIR="/opt/muos/app/dingux"
 
 cd "$DINGUX_DIR" || exit
 
-SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "$DINGUX_DIR/gamecontrollerdb.txt") ./dingux --config "$DINGUX_DIR/commander.cfg"
+export LD_LIBRARY_PATH=/usr/lib32
 
+SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "$DINGUX_DIR/gamecontrollerdb.txt") ./dingux --config "$DINGUX_DIR/dingux.cfg"
--- app/gmu.sh
+++ app/gmu.sh
@@ -4,5 +4,7 @@ GMU_DIR="/opt/muos/app/gmu"
 
 cd "$GMU_DIR" || exit
 
+export LD_LIBRARY_PATH=/usr/lib32
+
 SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "$GMU_DIR/gamecontrollerdb.txt") ./gmu -d "$GMU_DIR" -c "$GMU_DIR/gmu.conf"
 

2c1a29b5dead026a3f35e86f2515f7679a73de62
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-05-02 21:07:02 +1000

    muOS Internals - Adapt archive extraction to muxlog

--- script/mux/extract.sh
+++ script/mux/extract.sh
@@ -1,32 +1,67 @@
 #!/bin/sh
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <archive>"
-    exit 1
+	echo "Usage: $0 <archive>"
+	exit 1
 fi
 
 if [ ! -e "$1" ]; then
-    echo "Error: Archive '$1' not found"
-    exit 1
+	echo "Error: Archive '$1' not found"
+	exit 1
 fi
 
-ARCHIVE_NAME="${1##*/}"
-MUX_TEMP="/tmp/muxtmp"
+# Suspend the muxarchive program
+pkill -STOP muxarchive
 
+# Start the muxlog program
+/opt/muos/extra/muxlog &
+
+ARCHIVE_NAME="${1##*/}"
+MUX_TEMP="/opt/muxtmp"
+
+rm -rf $MUX_TEMP
 mkdir $MUX_TEMP
-unzip -o "$1" -d "$MUX_TEMP/"
+
+# Function to push messages to muxlog
+push_to_muxlog() {
+	while read -r line; do
+		echo "$line" > /tmp/muxlog
+	done
+}
+
+unzip -o "$1" -d "$MUX_TEMP/" 2>&1 | push_to_muxlog &
+
+# Wait for unzip to finish
+wait $!
+
+echo "Copying Files" > /tmp/muxlog
 cp -rf $MUX_TEMP/* /
+
+echo "Correcting Permissions" > /tmp/muxlog
 chmod -R 755 /opt/muos
+
+echo "Removing Temporary Storage" > /tmp/muxlog
 rm -Rf $MUX_TEMP
 
 UPDATE_SCRIPT=/opt/update.sh
 if [ -s "$UPDATE_SCRIPT" ]; then
-	chmod 755 "$UPDATE_SCRIPT"
-	."$UPDATE_SCRIPT"
-	rm "$UPDATE_SCRIPT"
+        echo "Running Script"
+        chmod 755 "$UPDATE_SCRIPT"
+        ."$UPDATE_SCRIPT"
+        rm "$UPDATE_SCRIPT"
 fi
 
 touch "/mnt/mmc/MUOS/update/installed/$ARCHIVE_NAME.done"
 
+echo "Filesystem Sync" > /tmp/muxlog
 sync
+sleep 5
+
+echo "All done!" > /tmp/muxlog
+sleep 1
+
+echo "!end" > /tmp/muxlog
+
+# Resume the muxbackup program
+pkill -CONT muxarchive
 

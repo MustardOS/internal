8a09ff1273c7e458bbec01252f282a351c336779
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-05-03 22:12:52 +1000

    muOS Internals - Fixed extraction method to read directly from tempfile

--- script/mux/extract.sh
+++ script/mux/extract.sh
@@ -10,58 +10,72 @@ if [ ! -e "$1" ]; then
 	exit 1
 fi
 
-# Suspend the muxarchive program
 pkill -STOP muxarchive
 
-# Start the muxlog program
+echo " " > /tmp/muxlog_msg
 /opt/muos/extra/muxlog &
 
 ARCHIVE_NAME="${1##*/}"
 MUX_TEMP="/opt/muxtmp"
 
-rm -rf $MUX_TEMP
-mkdir $MUX_TEMP
+rm -rf "$MUX_TEMP"
+mkdir "$MUX_TEMP"
 
-# Function to push messages to muxlog
+TMP_FILE=$(mktemp)
+
 push_to_muxlog() {
-	while read -r line; do
-		echo "$line" > /tmp/muxlog
+	current_line=""
+	while true; do
+		if [ -s "$TMP_FILE" ]; then
+			new_line=$(tail -n 1 "$TMP_FILE" | sed 's/^[[:space:]]*//')
+			if [ "$new_line" != "$current_line" ]; then
+				echo "$new_line" > /tmp/muxlog_info
+				current_line="$new_line"
+			fi
+		fi
 	done
 }
+push_to_muxlog &
 
-unzip -o "$1" -d "$MUX_TEMP/" 2>&1 | push_to_muxlog &
-
-# Wait for unzip to finish
+echo "Extracting Archive" > /tmp/muxlog_msg
+unzip -o "$1" -d "$MUX_TEMP/" > "$TMP_FILE" 2>&1 &
 wait $!
 
-echo "Copying Files" > /tmp/muxlog
-cp -rf $MUX_TEMP/* /
+rm -rf /tmp/tmp.*
 
-echo "Correcting Permissions" > /tmp/muxlog
+echo "Copying Files" > /tmp/muxlog_msg
+echo " " > /tmp/muxlog_info
+cp -rf "$MUX_TEMP"/* /
+
+echo "Correcting Permissions" > /tmp/muxlog_msg
+echo " " > /tmp/muxlog_info
 chmod -R 755 /opt/muos
 
-echo "Removing Temporary Storage" > /tmp/muxlog
-rm -Rf $MUX_TEMP
+echo "Removing Temporary Storage" > /tmp/muxlog_msg
+echo " " > /tmp/muxlog_info
+rm -Rf "$MUX_TEMP"
 
 UPDATE_SCRIPT=/opt/update.sh
 if [ -s "$UPDATE_SCRIPT" ]; then
-        echo "Running Script"
-        chmod 755 "$UPDATE_SCRIPT"
-        ."$UPDATE_SCRIPT"
-        rm "$UPDATE_SCRIPT"
+	echo "Running Script" > /tmp/muxlog_msg
+	echo " " > /tmp/muxlog_info
+	chmod 755 "$UPDATE_SCRIPT"
+	./"$UPDATE_SCRIPT"
+	rm "$UPDATE_SCRIPT"
 fi
 
 touch "/mnt/mmc/MUOS/update/installed/$ARCHIVE_NAME.done"
 
-echo "Filesystem Sync" > /tmp/muxlog
+echo "Filesystem Sync" > /tmp/muxlog_msg
+echo " " > /tmp/muxlog_info
 sync
-sleep 5
 
-echo "All done!" > /tmp/muxlog
+echo "All done!" > /tmp/muxlog_msg
+echo " " > /tmp/muxlog_info
 sleep 1
 
-echo "!end" > /tmp/muxlog
+echo "!end" > /tmp/muxlog_info
 
-# Resume the muxbackup program
 pkill -CONT muxarchive
+pkill -f extract.sh
 

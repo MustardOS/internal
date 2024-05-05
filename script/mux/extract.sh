c45807fea70303601a30428eb6c0741d4a7917a5
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-05-05 21:05:40 +1000

    muOS Internals - Fixed archive installed file check - Updated ScummVM

--- script/mux/extract.sh
+++ script/mux/extract.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <archive>"
-    exit 1
+	echo "Usage: $0 <archive>"
+	exit 1
 fi
 
 if [ ! -e "$1" ]; then
-    echo "Error: Archive '$1' not found"
-    exit 1
+	echo "Error: Archive '$1' not found"
+	exit 1
 fi
 
 pkill -STOP muxarchive
@@ -30,22 +30,22 @@ unzip -o "$1" -d "$MUX_TEMP/" > "$TMP_FILE" 2>&1 &
 
 C_LINE=""
 while true; do
-    IS_WORKING=$(ps aux | grep '[u]nzip' | awk '{print $1}')
+	IS_WORKING=$(ps aux | grep '[u]nzip' | awk '{print $1}')
 
-    if [ -s "$TMP_FILE" ]; then
-        N_LINE=$(tail -n 1 "$TMP_FILE" | sed 's/^[[:space:]]*//')
-        if [ "$N_LINE" != "$C_LINE" ]; then
-            echo "$N_LINE"
-            echo "$N_LINE" > /tmp/muxlog_info
-            C_LINE="$N_LINE"
-        fi
-    fi
+	if [ -s "$TMP_FILE" ]; then
+		N_LINE=$(tail -n 1 "$TMP_FILE" | sed 's/^[[:space:]]*//')
+		if [ "$N_LINE" != "$C_LINE" ]; then
+			echo "$N_LINE"
+			echo "$N_LINE" > /tmp/muxlog_info
+			C_LINE="$N_LINE"
+		fi
+	fi
 
-    if [ -z "$IS_WORKING" ]; then
-        break
-    fi
-    
-    sleep 0.25
+	if [ -z "$IS_WORKING" ]; then
+		break
+	fi
+	
+	sleep 0.25
 done
 
 echo "Copying Files" > /tmp/muxlog_info
@@ -56,16 +56,17 @@ chmod -R 755 /opt/muos
 
 UPDATE_SCRIPT=/opt/update.sh
 if [ -s "$UPDATE_SCRIPT" ]; then
-    echo "Running Update Script" > /tmp/muxlog_info
-    chmod 755 "$UPDATE_SCRIPT"
-    ./"$UPDATE_SCRIPT"
-    rm "$UPDATE_SCRIPT"
+	echo "Running Update Script" > /tmp/muxlog_info
+	chmod 755 "$UPDATE_SCRIPT"
+	./"$UPDATE_SCRIPT"
+	rm "$UPDATE_SCRIPT"
 fi
 
 echo "Sync Filesystem" > /tmp/muxlog_info
 sync
 
 echo "All Done!" > /tmp/muxlog_info
+touch "/mnt/mmc/MUOS/update/installed/$ARCHIVE_NAME.done"
 sleep 1
 
 killall -q muxlog

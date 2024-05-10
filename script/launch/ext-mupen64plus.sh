3346916526bf22016c088ef2951e6ab9ebcc6f4c
antiKk <antikk@hobbiton.lan>
2024-05-11 09:14:18 +1000

    Fixed N64 hanging on launch - corey. Removed PPSSPP FPS display.

--- script/launch/ext-mupen64plus.sh
+++ script/launch/ext-mupen64plus.sh
@@ -26,7 +26,7 @@ fi
 chmod +x $EMUDIR/mupen64plus
 cd $EMUDIR || continue
 
-fbset -fb /dev/fb0 -g 320 240 320 240 32
+fbset -fb /dev/fb0 -g 320 240 320 480 32
 
 HOME="$EMUDIR" SDL_ASSERT=always_ignore ./mupen64plus --corelib ./libmupen64plus.so.2.0.0 --configdir . "$ROM"
 

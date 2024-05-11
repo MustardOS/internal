47f27051f7a951881021a3a0384985b3c1b85b62
antiKk <4548266+antiKk@users.noreply.github.com>
2024-05-11 23:51:20 +1000

    Fixed Wifi Glyph (again). Set Mupen Rice as default N64 (#22)
    Co-authored-by: antiKk <antikk@hobbiton.lan>

--- script/web/ping.sh
+++ script/web/ping.sh
@@ -6,26 +6,25 @@ CONFIG=/opt/muos/config/config.txt
 P_COUNT=0
 
 while true; do
-	NET_ENABLED=$(parse_ini "$CONFIG" "network" "enabled")
-	NET_DNS=$(parse_ini "$CONFIG" "network" "dns")
+    NET_ENABLED=$(parse_ini "$CONFIG" "network" "enabled")
+    NET_DNS=$(parse_ini "$CONFIG" "network" "dns")
 
-	NET_VISUAL=$(parse_ini "$CONFIG" "visual" "network")
-	
-	if [ "$NET_ENABLED" -eq 1 ] && [ "$NET_VISUAL" -eq 1 ]; then
-		if [ "$P_COUNT" -eq 10 ]; then
-			if ping -q -c 1 "$NET_DNS" > /dev/null; then
-				echo "1" > /tmp/mux_ping
-			else
-				echo "0" > /tmp/mux_ping
-			fi
-			P_COUNT=0
-		fi
-		P_COUNT=$P_COUNT+1
-	else
-		echo "0" > /tmp/mux_ping
-		P_COUNT=0
-	fi
+    NET_VISUAL=$(parse_ini "$CONFIG" "visual" "network")
+    
+    if [ "$NET_ENABLED" -eq 1 ] && [ "$NET_VISUAL" -eq 1 ]; then
+        if [ "$P_COUNT" -eq 10 ]; then
+            if ping -q -c 1 "$NET_DNS" > /dev/null; then
+                echo "1" > /tmp/mux_ping
+            else
+                echo "0" > /tmp/mux_ping
+            fi
+            P_COUNT=0
+        fi
+        P_COUNT=$((P_COUNT + 1))
+    else
+        echo "0" > /tmp/mux_ping
+        P_COUNT=0
+    fi
 
-	sleep 2
+    sleep 2
 done &
-

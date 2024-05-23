7b6f96cb26ced734810cdcc64cda1784378f8663
antiKk <4548266+antiKk@users.noreply.github.com>
2024-05-23 20:39:37 +1000

    Added Simple Terminal. Fixed Dingux rotation on 28XX

--- app/dingux.sh
+++ app/dingux.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Check for RG28XX and rotate screen if found
+if [ "$(cat /opt/muos/config/device.txt)" = "RG28XX" ]; then
+	export SDL_HQ_SCALER=1
+fi
+
 DINGUX_DIR="/opt/muos/app/dingux"
 
 cd "$DINGUX_DIR" || exit

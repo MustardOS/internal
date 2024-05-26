fe67fa6945eeebd329f2d834515d1fcaae6535c9
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-05-26 20:19:28 +1000

    muOS Internals - Agnostic Branch - Fixed wpa_passphrase error

--- script/web/password.sh
+++ script/web/password.sh
@@ -5,12 +5,7 @@ PASS=$(cat "/tmp/net_pass")
 
 WPA_CONFIG=/etc/wpa_supplicant.conf
 
-#echo "ctrl_interface=/var/run/wpa_supplicant" > "$WPA_CONFIG"
-#echo "ap_scan=1" >> "$WPA_CONFIG"
-#echo "ieee80211w=1" >> "$WPA_CONFIG"
-#echo "" >> "$WPA_CONFIG"
-
-/usr/sbin/wpa_passphrase "$SSID" "$PASS" >> "$WPA_CONFIG"
+wpa_passphrase "$SSID" "$PASS" > "$WPA_CONFIG"
 
 sed -i '3d' "$WPA_CONFIG"
 

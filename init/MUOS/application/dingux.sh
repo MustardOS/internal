e5a07352086d1c5ceb609aca19bacf18a4127683
antiKk <antikk@hobbiton.lan>
2024-05-27 22:16:28 +1000

    Updated app scripts to reference new location

--- init/MUOS/application/dingux.sh
+++ init/MUOS/application/dingux.sh
@@ -5,7 +5,6 @@
 DEVICE=$(tr '[:upper:]' '[:lower:]' < "/opt/muos/config/device.txt")
 DEVICE_CONFIG="/opt/muos/device/$DEVICE/config.ini"
 
-STORE_ROM=$(parse_ini "$DEVICE_CONFIG" "storage.rom" "mount")
 SDL_SCALER=$(parse_ini "$DEVICE_CONFIG" "sdl" "scaler")
 
 export SDL_HQ_SCALER="$SDL_SCALER"

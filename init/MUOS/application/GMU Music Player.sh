b269fec98befb09494e653e2f86e1c6ad169223b
antiKk <4548266+antiKk@users.noreply.github.com>
2024-06-08 15:16:04 +1000

    Fix GMU Launch Script. Fix lconfig in startup. (#59)
    * Fix GMU Launch Script. Fix lconfig in startup.
    
    * Fix GMU Launch Script. Fix lconfig in startup.
    
    ---------
    
    Co-authored-by: antiKk <antikk@hobbiton.lan>

--- init/MUOS/application/GMU Music Player.sh
+++ init/MUOS/application/GMU Music Player.sh
@@ -21,18 +21,17 @@ STORE_ROM=$(parse_ini "$DEVICE_CONFIG" "storage.rom" "mount")
 SDL_SCALER=$(parse_ini "$DEVICE_CONFIG" "sdl" "scaler")
 
 GMU_DIR="$STORE_ROM/MUOS/application/.gmu"
-GPTOKEYB="$STORE_ROM/MUOS/emulator/gptokeyb/gptokeyb2"
+GPTOKEYB="$STORE_ROM/MUOS/emulator/gptokeyb/gptokeyb2.armhf"
 
 cd "$GMU_DIR" || exit
 
-export SDL_GAMECONTROLLER="/usr/lib32/gamecontrollerdb.txt"
-export LD_LIBRARY_PATH=/usr/lib32
+export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib32/gamecontrollerdb.txt"
 export HOME=/root
+export LD_LIBRARY_PATH=/usr/lib32
 
 echo "gmu" > /tmp/fg_proc
 
 $GPTOKEYB "gmu" -c "$GMU_DIR/gmu.gptk" &
-SDL_ASSERT=always_ignore $SDL_GAMECONTROLLER ./gmu -d "$GMU_DIR" -c "$GMU_DIR/gmu.conf"
+SDL_ASSERT=always_ignore $SDL_GAMECONTROLLERCONFIG ./gmu -d "$GMU_DIR" -c "$GMU_DIR/gmu.conf"
 
-kill -9 $(pidof gptokeyb2)
-
+kill -9 $(pidof gptokeyb2.armhf)

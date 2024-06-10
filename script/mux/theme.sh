769e0b925b46cb48124143f16a2d963c72df44d6
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-06-10 18:08:03 +1000

    muOS Internals - Added random theme script - Fixed RG35XX-H analog config

--- script/mux/theme.sh
+++ script/mux/theme.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 <theme>"
-	exit 1
-fi
-
-THEME="$1"
-
 . /opt/muos/script/system/parse.sh
 CONFIG=/opt/muos/config/config.ini
+
+RANDOM_THEME=$(parse_ini "$CONFIG" "settings.advanced" "random_theme")
 
 DEVICE=$(tr '[:upper:]' '[:lower:]' < "/opt/muos/config/device.txt")
 DEVICE_CONFIG="/opt/muos/device/$DEVICE/config.ini"
@@ -16,15 +11,25 @@ DEVICE_CONFIG="/opt/muos/device/$DEVICE/config.ini"
 STORE_BOOT=$(parse_ini "$DEVICE_CONFIG" "storage.boot" "mount")
 STORE_ROM=$(parse_ini "$DEVICE_CONFIG" "storage.rom" "mount")
 
-THEMEDIR="$STORE_ROM/MUOS/theme/active"
+if [ "$RANDOM_THEME" -eq 1 ]; then
+	THEME=$(ls "$STORE_ROM"/MUOS/theme/*.zip | shuf -n 1)
+else
+	if [ "$#" -ne 1 ]; then
+		echo "Usage: $0 <theme>"
+		exit 1
+	fi
+	THEME="$STORE_ROM/MUOS/theme/$1.zip"
+fi
+
+THEME_DIR="$STORE_ROM/MUOS/theme"
 
 BOOTLOGO_DEF="/opt/muos/device/$DEVICE/bootlogo.bmp"
-BOOTLOGO_NEW="$THEMEDIR/image/bootlogo.bmp"
+BOOTLOGO_NEW="$THEME_DIR/active/image/bootlogo.bmp"
 
 cp "$BOOTLOGO_DEF" "$STORE_BOOT/bootlogo.bmp"
 
-rm -rf "$THEMEDIR"
-unzip "$STORE_ROM/MUOS/theme/$THEME" -d "$THEMEDIR"
+rm -rf "$THEME_DIR/active"
+unzip "$THEME" -d "$THEME_DIR/active"
 
 if [ -f "$BOOTLOGO_NEW" ]; then
 	cp "$BOOTLOGO_NEW" "$STORE_BOOT/bootlogo.bmp"

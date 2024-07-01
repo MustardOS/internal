f4134a181bb75bace8fe92c01dab64a9fbc1d5d8
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-07-01 21:06:44 +1000

    muOS Internals - Disabled audio and brightness adjustments if HDMI is in use

--- device/rg35xx-2024/input/combo/audio.sh
+++ device/rg35xx-2024/input/combo/audio.sh
@@ -3,6 +3,7 @@
 . /opt/muos/script/var/func.sh
 
 . /opt/muos/script/var/device/audio.sh
+. /opt/muos/script/var/device/screen.sh
 
 VOLUME_FILE="/opt/muos/config/volume.txt"
 VOLUME_FILE_PERCENT="/tmp/current_volume_percent"
@@ -29,43 +30,45 @@ SET_CURRENT() {
 }
 
 if [ -z "$1" ]; then
-	PERCENTAGE=$(awk "BEGIN {printf \"%d\", ($CURRENT_VL/$MAX_BRIGHT)*100}")
+	PERCENTAGE=$(awk "BEGIN {printf \"%d\", ($CURRENT_VL/$DC_SND_MAX)*100}")
 	echo "Volume is $CURRENT_VL ($PERCENTAGE%)"
 	exit 0
 fi
 
-case "$1" in
-	U)
-		NEW_VL=$((CURRENT_VL + 2))
-		if [ "$NEW_VL" -lt $DC_SND_MIN ]; then
-			NEW_VL=$DC_SND_MIN
-		fi
-		if [ "$NEW_VL" -gt "$DC_SND_MAX" ]; then
-			NEW_VL=$DC_SND_MAX
-		fi
-		SLEEP_STATE_VAL=$(cat "$SLEEP_STATE")
-		if [ "$SLEEP_STATE_VAL" = "awake" ]; then
-			SET_CURRENT "$NEW_VL"
-		fi
-		;;
-	D)
-		NEW_VL=$((CURRENT_VL - 2))
-		if [ "$NEW_VL" -lt $DC_SND_MIN ]; then
-			NEW_VL=0
-		fi
-		SLEEP_STATE_VAL=$(cat "$SLEEP_STATE")
-		if [ "$SLEEP_STATE_VAL" = "awake" ]; then
-			SET_CURRENT "$NEW_VL"
-		fi
-		;;
-	[0-9]*)
-		if [ "$1" -ge 0 ] && [ "$1" -le "$DC_SND_MAX" ]; then
-			SET_CURRENT "$1"
-		else
-			printf "Invalid volume value\n\tMinimum is $DC_SND_MIN\n\tMaximum is $DC_SND_MAX\n"
-		fi
-		;;
-	*)
-		printf "Invalid Argument\n\tU) Increase Brightness\n\tD) Decrease Brightness\n"
-		;;
-esac
+if [ ! "$(cat "$DC_SCR_HDMI")" = "HDMI=1" ]; then
+	case "$1" in
+		U)
+			NEW_VL=$((CURRENT_VL + 2))
+			if [ "$NEW_VL" -lt "$DC_SND_MIN" ]; then
+				NEW_VL=$DC_SND_MIN
+			fi
+			if [ "$NEW_VL" -gt "$DC_SND_MAX" ]; then
+				NEW_VL=$DC_SND_MAX
+			fi
+			SLEEP_STATE_VAL=$(cat "$SLEEP_STATE")
+			if [ "$SLEEP_STATE_VAL" = "awake" ]; then
+				SET_CURRENT "$NEW_VL"
+			fi
+			;;
+		D)
+			NEW_VL=$((CURRENT_VL - 2))
+			if [ "$NEW_VL" -lt "$DC_SND_MIN" ]; then
+				NEW_VL=0
+			fi
+			SLEEP_STATE_VAL=$(cat "$SLEEP_STATE")
+			if [ "$SLEEP_STATE_VAL" = "awake" ]; then
+				SET_CURRENT "$NEW_VL"
+			fi
+			;;
+		[0-9]*)
+			if [ "$1" -ge 0 ] && [ "$1" -le "$DC_SND_MAX" ]; then
+				SET_CURRENT "$1"
+			else
+				printf "Invalid volume value\n\tMinimum is %s\n\tMaximum is %s\n" "$DC_SND_MAX" "$DC_SND_MAX"
+			fi
+			;;
+		*)
+			printf "Invalid Argument\n\tU) Increase Brightness\n\tD) Decrease Brightness\n"
+			;;
+	esac
+fi

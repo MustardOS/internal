9deb8d0810b84d871c86f4a14112498d68f12f6a
cmclark00 <cmclark00@gmail.com>
2024-07-04 00:33:15 -0400

    Fixed ppsspp controls across all devices

--- init/MUOS/application/PPSSPP.sh
+++ init/MUOS/application/PPSSPP.sh
@@ -16,13 +16,12 @@ echo app >/tmp/act_go
 . /opt/muos/script/var/device/storage.sh
 . /opt/muos/script/var/device/sdl.sh
 
+PPSSPP_DIR="$DC_STO_ROM_MOUNT/MUOS/emulator/ppsspp"
+
 export SDL_HQ_SCALER="$DC_SDL_SCALER"
 export SDL_ROTATION="$DC_SDL_ROTATION"
 export SDL_BLITTER_DISABLED="$DC_SDL_BLITTER_DISABLED"
-export SDL_GAMECONTROLLER="$PPSSPP_DIR/gamecontrollerdb.txt"
-export HOME=/root
-
-PPSSPP_DIR="$DC_STO_ROM_MOUNT/MUOS/emulator/ppsspp"
+export HOME=$PPSSPP_DIR
 
 cd "$PPSSPP_DIR" || exit
 
@@ -34,7 +33,7 @@ else
 	fbset -fb /dev/fb0 -g 960 720 960 1440 32
 fi
 
-SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "$SDL_GAMECONTROLLER") ./PPSSPP
+SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "/usr/lib/gamecontrollerdb.txt") ./PPSSPP
 
 if [ "$DEVICE_TYPE" = "rg28xx" ]; then
     fbset -fb /dev/fb0 -g 480 640 480 1280 32
@@ -42,3 +41,6 @@ else
     fbset -fb /dev/fb0 -g 640 480 640 960 32
 fi
 
+unset SDL_HQ_SCALER
+unset SDL_ROTATION
+unset SDL_BLITTER_DISABLED

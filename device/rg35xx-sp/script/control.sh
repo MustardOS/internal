110dbf6d1accb97e4a67bb0b18f6b25b391f5640
antiKk <4548266+antiKk@users.noreply.github.com>
2024-07-05 10:48:14 +1000

    Fix conflict with PPSSPP change.

--- device/rg35xx-sp/script/control.sh
+++ device/rg35xx-sp/script/control.sh
@@ -7,10 +7,10 @@
 RMP_LOG="/mnt/mmc/MUOS/log/device.log"
 LOG_DATE="$(date +'[%Y-%m-%d]')"
 
-# Move Moonlight configurations
-ML_CONF="$DC_STO_ROM_MOUNT/MUOS/application/.moonlight/moonlight.conf"
-if [ ! -f "$ML_CONF" ]; then
-	cp "$DEVICE_CONTROL_DIR/moonlight.conf" "$ML_CONF"
+# Move control.ini for ppsspp standalone
+CONTROL_INI="$DC_STO_ROM_MOUNT/MUOS/emulator/ppsspp/.config/ppsspp/PSP/SYSTEM/controls.ini"
+if [ ! -f "$CONTROL_INI" ]; then
+	cp "$DEVICE_CONTROL_DIR/controls.ini" "$CONTROL_INI"
 fi
 
 # Move RetroArch configurations

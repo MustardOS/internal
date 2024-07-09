1f688b05fbabe199f637c11c4fdf1e2641793b48
Duncanyoyo1 <duncanyoyo1@gmail.com>
2024-07-08 21:19:15 -0400

    Fix Carts

--- script/launch/ext-pico8.sh
+++ script/launch/ext-pico8.sh
@@ -17,29 +17,7 @@ echo "pico8_64" >/tmp/fg_proc
 
 GPTOKEYB="$DC_STO_ROM_MOUNT/MUOS/emulator/gptokeyb/gptokeyb2"
 
-_BACKUPFAV=$(
-	cat <<EOF
-#!/bin/sh 
-
-P8_DIR="$STORE_ROM/MUOS/emulator/pico8/.lexaloffle/pico-8"
-FAVES="$P8_DIR/favourites.txt"
-CARTS="$P8_DIR/bbs/carts"
-
-DEST=$(dirname "$0")
-
-while IFS="|" read -r line
-do
-	filename=$(echo "$line" | cut -d'|' -f2)
-	displayname=$(echo "$line" | cut -d'|' -f7)
-	trimmed_filename=$(echo "$filename" | xargs)
-	tidy_display=$(echo "$displayname" | xargs)
-	cp $CARTS/$trimmed_filename.p8.png "$DEST/$tidy_display.p8.png"
-done < "$FAVES"
-EOF
-) >"$R_DIR"/"Backup Favourites.sh"
-
 EMUDIR="$DC_STO_ROM_MOUNT/MUOS/emulator/pico8"
-HOME="$EMUDIR"
 
 chmod +x "$EMUDIR"/wget
 chmod +x "$EMUDIR"/pico8_64
@@ -49,11 +27,9 @@ cd "$EMUDIR" || exit
 if [ "$NAME" = "Splore" ]; then
 	PATH="$EMUDIR:$PATH" HOME="$EMUDIR" SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "/usr/lib/gamecontrollerdb.txt") $GPTOKEYB "./pico8_64" -c "./pico8.gptk" &
 ./pico8_64 -windowed 0 -splore
-elif [ "$NAME" = "Backup Favourites" ]; then
-	./"$ROM"
 else
 	PATH="$EMUDIR:$PATH" HOME="$EMUDIR" SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "/usr/lib/gamecontrollerdb.txt") $GPTOKEYB "./pico8_64" -c "./pico8.gptk" &
-./pico8_64 -windowed 0 -run "$ROM"
+./pico8_64 -windowed 0 -run $ROM
 fi
 kill -9 "$(pidof pico8_64)"
 kill -9 "$(pidof gptokeyb2)"

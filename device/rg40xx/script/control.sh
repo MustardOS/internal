bd875e98828b9be6e04870d0c7a3c301ed78c20a
cmclark00 <116466213+cmclark00@users.noreply.github.com>
2024-07-18 05:52:15 -0400

    Device Specific N64 Controls and Reorganization (#116)
    * Device Specific N64 Controls and Reorganization
    * RG40XXH N64 support hopefully
    * Fix mupen cfg names
    * fix ra controls, remove mupen64plus-next .rmp file as its no longer needed

--- device/rg40xx/script/control.sh
+++ device/rg40xx/script/control.sh
@@ -13,6 +13,18 @@ if [ ! -f "$CONTROL_INI" ]; then
 	cp "$DEVICE_CONTROL_DIR/controls.ini" "$CONTROL_INI"
 fi
 
+# Move mupen64plus-rice.cfg for external mupen64plus
+MP64RICE="$DC_STO_ROM_MOUNT/MUOS/emulator/mupen64plus/mupen64plus-rice.cfg"
+if [ ! -f "$MP64RICE" ]; then
+	cp "$DEVICE_CONTROL_DIR/mupen64plus-rice.cfg" "$MP64RICE"
+fi
+
+# Move mupen64plus-gl64.cfg for external mupen64plus
+MP64GL64="$DC_STO_ROM_MOUNT/MUOS/emulator/mupen64plus/mupen64plus-gl64.cfg"
+if [ ! -f "$MP64GL64" ]; then
+	cp "$DEVICE_CONTROL_DIR/mupen64plus-gl64.cfg" "$MP64GL64"
+fi
+
 # Move RetroArch configurations
 RA_CONF="$DC_STO_ROM_MOUNT/MUOS/retroarch/retroarch.cfg"
 if [ ! -f "$RA_CONF" ]; then
@@ -27,57 +39,6 @@ fi
 
 # Move DraStic configuration
 cp -f "$DEVICE_CONTROL_DIR/drastic.cfg" "$DC_STO_ROM_MOUNT/MUOS/emulator/drastic/config/drastic.cfg"
-
-# Move Mupen configuration
-MUP_DEF="$DC_STO_ROM_MOUNT/MUOS/emulator/mupen64plus/mupen64plus.cfg"
-MUP_RICE="$DC_STO_ROM_MOUNT/MUOS/emulator/mupen64plus/mupen64plus-rice.cfg"
-if [ ! -f "$MUP_RICE" ]; then
-	cp "$DC_STO_ROM_MOUNT/MUOS/emulator/mupen64plus/mupen64plus-rice-plus.cfg" "$MUP_RICE"
-	# Set as initial default core
-	cp "$MUP_RICE" "$MUP_DEF"
-fi
-
-MUP_GL64="$DC_STO_ROM_MOUNT/MUOS/emulator/mupen64plus/mupen64plus-gl64.cfg"
-if [ ! -f "$MUP_GL64" ]; then
-	cp "$DC_STO_ROM_MOUNT/MUOS/emulator/mupen64plus/mupen64plus-gl64-plus.cfg" "$MUP_GL64"
-fi
-
-# Define Nintendo 64 remap paths
-MP64_RMP="$DC_STO_ROM_MOUNT/MUOS/info/config/remaps/Mupen64Plus-Next/Mupen64Plus-Next.rmp"
-
-# Check for Mupen64Plus remap
-MP64_DIR=$(dirname "$MP64_RMP")
-if [ ! -d "$MP64_DIR" ]; then
-	mkdir -p "$MP64_DIR"
-fi
-
-if [ ! -e "$MP64_RMP" ]; then
-	cat <<EOF >"$MP64_RMP"
-input_libretro_device_p1 = "1"
-input_libretro_device_p2 = "1"
-input_libretro_device_p3 = "1"
-input_libretro_device_p4 = "1"
-input_player1_analog_dpad_mode = "0"
-input_player1_btn_down = "18"
-input_player1_btn_left = "17"
-input_player1_btn_right = "16"
-input_player1_btn_up = "19"
-input_player1_stk_l_x+ = "7"
-input_player1_stk_l_x- = "6"
-input_player1_stk_l_y+ = "5"
-input_player1_stk_l_y- = "4"
-input_player2_analog_dpad_mode = "0"
-input_player3_analog_dpad_mode = "0"
-input_player4_analog_dpad_mode = "0"
-input_remap_port_p1 = "0"
-input_remap_port_p2 = "1"
-input_remap_port_p3 = "2"
-input_remap_port_p4 = "3"
-EOF
-	echo "$LOG_DATE File $MP64_RMP created. Set Mupen64Plus-Next controls for dpad only." >>"$RMP_LOG"
-else
-	echo "$LOG_DATE No file created. Remap existed at $MP64_RMP" >>"$RMP_LOG"
-fi
 
 # Define Playstation remap paths
 DUCK_RMP="$DC_STO_ROM_MOUNT/MUOS/info/config/remaps/DuckStation/DuckStation.rmp"

0b8dd08052311210ce863da1570362ef248c4459
cmclark00 <cmclark00@gmail.com>
2024-07-29 14:30:21 -0700

    fixed typo

--- script/launch/ext-drastic-steward.sh
+++ script/launch/ext-drastic-steward.sh
@@ -16,7 +16,7 @@ export SDL_ROTATION="$DC_SDL_ROTATION"
 export SDL_BLITTER_DISABLED="$DC_SDL_BLITTER_DISABLED"
 
 pkill -9 golden.sh
-pkill -9pw-play
+pkill -9 pw-play
 echo "Switching to ALSA-only configuration..."
 cp /etc/asound.conf /etc/asound.conf.bak
 cp /etc/asound.conf.alsa /etc/asound.conf

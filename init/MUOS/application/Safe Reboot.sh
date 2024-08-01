8ad3b598c6dc0ef4021a84f37bb846d2660e583f
Jonathan Rascher <jon@bcat.name>
2024-07-31 20:22:21 -0500

    Add debug utils to trigger clean shutdown/reboot
    They wrap the calls in fbpad, which halt.sh specifically looks for and
    doesn't kill. This allows the full shutdown script output (all the way
    up to the final `poweroff -f` or `reboot -f`) to be seen and recorded.
    (Probably we don't need this long term, but it's handy for testing.)

---
+++ init/MUOS/application/Safe Reboot.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. /opt/muos/script/var/global/setting_general.sh
+
+# Clear saved IP address and last played game. (See
+# https://github.com/MustardOS/frontend/blob/main/muxlaunch/main.c.)
+: >/opt/muos/config/address.txt
+if [ "$GC_GEN_STARTUP" = resume ]; then
+	: >/opt/muos/config/lastplay.txt
+fi
+
+/opt/muos/bin/fbpad /opt/muos/script/system/halt.sh reboot

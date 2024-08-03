20eb8ddffdd858172601fb61caaf7989c154579a
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-08-03 19:07:53 +1000

    muOS Internals - Added item content to global configuration - Update internal theme scheme

--- script/var/global/visual.sh
+++ script/var/global/visual.sh
@@ -8,6 +8,8 @@
 : "${GC_VIS_BOXART:=1}"
 : "${GC_VIS_NAME:=0}"
 : "${GC_VIS_DASH:=0}"
+: "${GC_VIS_CDIR:=1}"
+: "${GC_VIS_CBIN:=1}"
 
 GC_VIS_BATTERY=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "battery")
 GC_VIS_NETWORK=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "network")
@@ -16,6 +18,8 @@ GC_VIS_CLOCK=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "clock")
 GC_VIS_BOXART=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "boxart")
 GC_VIS_NAME=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "name")
 GC_VIS_DASH=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "dash")
+GC_VIS_CDIR=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "contentfolder")
+GC_VIS_CBIN=$(PARSE_INI "$GLOBAL_CONFIG" "visual" "contentfile")
 
 export GC_VIS_BATTERY
 export GC_VIS_NETWORK
@@ -24,3 +28,5 @@ export GC_VIS_CLOCK
 export GC_VIS_BOXART
 export GC_VIS_NAME
 export GC_VIS_DASH
+export GC_VIS_CDIR
+export GC_VIS_CBIN

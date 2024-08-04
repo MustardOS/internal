bb25c92aa865c6ac91648e54f3ca2f27e974bcc3
antiKk <4548266+antiKk@users.noreply.github.com>
2024-08-04 16:23:00 +1000

    Added task to Clear SFTPGO Keys (#136)
    Co-authored-by: antiKk <antikk@hobbiton.lan>

---
+++ init/MUOS/task/Clear SFTPGO Keys.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script will remove the SFTPGO keys
+# These will be (re)generated on next boot.
+
+pkill -STOP muxtask
+
+SFTP_DIR="/opt/sftpgo"
+
+echo "Deleting SFTPGO Keys"
+rm -f "${SFTP_DIR:?}"/id_*
+
+echo "Sync Filesystem"
+sync
+
+echo "All Done!"
+echo "Please reboot your console."
+sleep 5
+
+pkill -CONT muxtask
+exit 0

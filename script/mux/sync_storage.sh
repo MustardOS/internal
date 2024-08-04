69e5332cdf49f0763f8293e9ee6e2e7c8959cc19
antiKk <4548266+antiKk@users.noreply.github.com>
2024-08-04 16:28:54 +1000

    Updated sync_storage.sh to accept arguments. (#141)
    Co-authored-by: antiKk <antikk@hobbiton.lan>

--- script/mux/sync_storage.sh
+++ script/mux/sync_storage.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+TO_SYNC=$1
+
 # Import muOS Functions
 . /opt/muos/script/var/func.sh
 
@@ -7,9 +9,12 @@
 
 . /opt/muos/script/var/global/storage.sh
 
-if [ ! $GC_STO_THEME = "$DC_STO_ROM_MOUNT" ]; then
-    if [ ! -d "$GC_STO_THEME/MUOS/theme/" ]; then
-        mkdir -p "$GC_STO_THEME/MUOS/theme/"
+# Sync Themes
+if [ "$TO_SYNC" = "theme" ]; then
+    if [ $GC_STO_THEME != "$DC_STO_ROM_MOUNT" ]; then
+        if [ ! -d "$GC_STO_THEME/MUOS/theme/" ]; then
+            mkdir -p "$GC_STO_THEME/MUOS/theme/"
+        fi
+        rsync -a "$DC_STO_ROM_MOUNT/MUOS/theme/" "$GC_STO_THEME/MUOS/theme/"
     fi
-    rsync -a "$DC_STO_ROM_MOUNT/MUOS/theme/" "$GC_STO_THEME/MUOS/theme/"
 fi

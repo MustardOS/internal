629cfaa91105cfce0ad7b91414b1f4d7292c367a
Jonathan Rascher <jon@bcat.name>
2024-08-06 07:01:07 -0500

    Fix early FUSE umount and wait a bit after SIGKILL (#145)

--- script/system/halt_internal.sh
+++ script/system/halt_internal.sh
@@ -1,40 +1,27 @@
 #!/bin/sh
 
-# Wraps killall5 and automatically omits the specified PID (if any).
-KILL_PROCS () {
-	if [ -n "$OMIT_PID" ]; then
-		killall5 "$@" -o "$OMIT_PID"
-	else
-		killall5 "$@"
-	fi
-}
-
-# Waits 5 seconds for processes we SIGTERM'd to die. The `killall5 -CONT` trick
-# is inspired by various old sysvinit scripts as a harmless way to check if any
-# processes we targeted with SIGTERM are still alive.
+# Waits 5 seconds for processes to die. The `killall5 -CONT` trick is inspired
+# by various old sysvinit scripts as a harmless way to check if any processes
+# we tried to kill are still alive.
+#
+# Any arguments are passed along to killall5. (Use `-o PID` args to omit
+# certain processes from the existence check.)
 WAIT_FOR_DEATH () {
 	printf 'Waiting for processes to terminate: '
-	for _ in $(seq 5); do
-		sleep 1
+	for _ in $(seq 10); do
+		# Note we sleep *before* the first check, so we always delay
+		# 500ms. This may not be strictly necessary, but seems prudent
+		# given the weird timing issues with shutdown.
+		sleep .5
 		# If killall5 didn't find any processes to signal, they must
 		# all have terminated already, and we can stop waiting.
-		if ! KILL_PROCS -CONT; then
+		if ! killall5 -CONT "$@"; then
 			printf 'done\n'
 			return
 		fi
 	done
-	# After waiting politely a few seconds, let SIGKILL take care of 'em.
 	printf 'timed out\n'
 }
-
-# Sanity check parameters.
-if [ "$#" -ne 2 ]; then
-	printf 'Wrong number of arguments (run halt.sh, not halt_internal.sh)\n' >&2
-	exit 1
-fi
-
-HALT_CMD="$1"
-OMIT_PID="$2"
 
 # Sanity check that SID = PID. (See note on setsid in halt.sh for rationale.)
 if [ "$(cut -d ' ' -f 6 /proc/self/stat)" -ne "$$" ]; then
@@ -42,32 +29,40 @@ if [ "$(cut -d ' ' -f 6 /proc/self/stat)" -ne "$$" ]; then
 	exit 1
 fi
 
+# Sanity check required parameters. Any additional args will be passed to
+# killall5 directly, allowing the caller to specify `-o PID` and safeguard
+# certain processes (e.g., FUSE mounts) from our initial termination pass.
+if [ "$#" -le 1 ]; then
+	printf 'Wrong number of arguments (run halt.sh, not halt_internal.sh)\n' >&2
+	exit 1
+fi
+
+HALT_CMD="$1"
+shift 1
+
 # Stop system services.
-printf 'Stopping init scripts...\n'
+printf 'Running shutdown scripts...\n'
 /etc/init.d/rcK
 
-# Terminate other processes before halting. This is essentially what init
-# should do for us, but for some reason, BusyBox shutdown often hangs. This
-# doesn't seem to. (Why? Not sure, but it may be because we wait for processes
-# to terminate, and don't unmount till they're gone.)
+# Terminate other processes, giving them some time to clean up.
 printf 'Sending SIGTERM to processes...\n'
-KILL_PROCS -TERM
-WAIT_FOR_DEATH
+killall5 -TERM "$@"
+WAIT_FOR_DEATH "$@"
 
 printf 'Sending SIGKILL to processes...\n'
-KILL_PROCS -KILL
+killall5 -KILL "$@"
+WAIT_FOR_DEATH "$@"
 
-# Disable swap (we don't actually have any, but it's standard in shutdown
-# sequences) and cleanly unmount filesystems to avoid fsck/chkdsk errors.
+# Disable swap. (It's not enabled by default, but just in case.)
 printf 'Disabling swap...\n'
 swapoff -a
 
+# Cleanly unmount filesystems to avoid fsck/chkdsk errors.
 printf 'Unmounting filesystems...\n'
-umount -ar
+umount -arv
 
 # Actually halt/shut down/reboot the system. Note the -f so the halt command
 # only calls sync and reboot under the hood, rather than signaling init to
 # start its own buggy shutdown sequence.
 printf 'Going down for %s...\n' "$HALT_CMD"
 "$HALT_CMD" -f
-

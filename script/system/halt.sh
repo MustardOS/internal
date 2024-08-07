9219266407dab2fb5e82511098657750dc15a6bb
Jonathan Rascher <jon@bcat.name>
2024-08-06 22:43:02 -0500

    Show splash screen on shutdown/reboot
    Also updates some confusing or overly verbose comments in halt.sh.

--- script/system/halt.sh
+++ script/system/halt.sh
@@ -1,37 +1,42 @@
 #!/bin/sh
 
 # muOS shutdown/reboot script. This behaves a bit better than BusyBox
-# poweroff/reboot commands, which tend to hang (unknown timing issue?), and
-# which also make some odd choices (e.g., unmounting disks before sending
-# SIGTERM to processes, so running programs can't save state to disk.)
+# poweroff/reboot commands, which also make some odd choices (e.g., unmounting
+# disks before killing processes, so running programs can't save state).
+
+. /opt/muos/script/var/global/storage.sh
 
 # We pass our arguments along to halt_internal.sh, and we also build a list of
-# additional arguments (`set -- "$@" ARGS...`) that halt_internal.sh forwards
-# further to killall5. Specifically, we use `-o PID` params to tell killall5
-# not to kill certain processes too early in the sequence.
+# additional arguments (`set -- "$@" ARGS...`) that are forwarded further to
+# killall5. Specifically, we add `-o PID` args below to avoid killing certain
+# processes too early in the sequence.
 case "$1" in
-	halt|poweroff|reboot) ;;
+	halt|poweroff)
+		SPLASH_IMG=shutdown
+		;;
+	reboot)
+		SPLASH_IMG=reboot
+		;;
 	*)
 		printf 'Usage: %s {halt|poweroff|reboot}\n' "$0" >&2
 		exit 1
 		;;
 esac
 
-# As a debugging aid, check if our parent process is fbpad so we can avoid
-# killing it. This gives us an easy way to see full script output up to the
-# final halt command.
 if [ "$(readlink "/proc/$PPID/exe")" = /opt/muos/bin/fbpad ]; then
+	# With verbose messages enabled, we're launched inside fbpad. Avoid
+	# prematurely killing it so console output remains visible.
 	set -- "$@" -o "$PPID"
+else
+	# Otherwise, show a theme-provided splash screen to give immediate
+	# visual feedback since the shutdown sequence can take a few seconds.
+	# (If muxsplash can't find the image, it simply clears the screen.)
+	/opt/muos/extra/muxsplash "$GC_STO_THEME/MUOS/theme/active/image/$SPLASH_IMG.png"
 fi
 
-# exFAT mounts are FUSE filesystems, and they unmount when their usermode FUSE
-# processes are terminated. By default, exFAT partitions would unmount in
-# parallel with cleanup that other programs do on SIGTERM, which could prevent
-# programs that write state to the SD cards from doing so on exit.
-#
-# To avoid that, we omit FUSE mount binaries from our termination process.
-# Instead, the FUSE filesystems are unmounted by `umount -a` just like the
-# kernel filesystems.
+# Omit FUSE mount binaries from the termination process. Otherwise, FUSE
+# filesystems (e.g., exFAT) would unmount in parallel with other programs
+# exiting, preventing them from writing state to the SD card during cleanup.
 for FUSE_PID in $(pidof /sbin/mount.exfat-fuse); do
 	set -- "$@" -o "$FUSE_PID"
 done
@@ -39,7 +44,7 @@ done
 # Our shutdown sequence kills processes using killall5, which sends signals to
 # every process except those in the current session. This means by default, we
 # might miss killing some processes (e.g., background jobs in the same shell as
-# halt.sh is invoked.)
+# halt.sh is invoked).
 #
 # We address this by wrapping the actual shutdown sequence in a setsid command,
 # ensuring we invoke killall5 from a new (and otherwise empty) session.

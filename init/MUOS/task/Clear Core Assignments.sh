8ea8c56d6bd0b66099aca9593dd8de6efdb3115a
antiKk <antikk@hobbiton.lan>
2024-08-13 20:31:25 +1000

    Added script to remove all core assignments.

---
+++ init/MUOS/task/Clear Core Assignments.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+. /opt/muos/script/var/func.sh
+
+. /opt/muos/script/var/global/storage.sh
+
+pkill -STOP muxtask
+
+MUOS_CORE_DIR="$GC_STO_CONFIG/MUOS/info/core"
+
+echo "Removing all core assignments"
+rm -rf "${MUOS_CORE_DIR:?}"/*
+
+echo "Sync Filesystem"
+sync
+
+echo "All Done!"
+sleep 2
+
+pkill -CONT muxtask
+exit 0

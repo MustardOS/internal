013c842bbef0a02a632940616b02173e10836cb5
Jonathan Rascher <jon@bcat.name>
2024-08-20 22:19:35 -0500

    Look for PICO-8 binary/data files in BIOS dir

--- script/launch/ext-pico8.sh
+++ script/launch/ext-pico8.sh
@@ -15,8 +15,15 @@ SET_VAR "system" "foreground_process" "pico8_64"
 GPTOKEYB="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/gptokeyb/gptokeyb2"
 EMUDIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/pico8"
 
+# First look for emulator in BIOS directory, which allows it to follow the
+# user's storage preference. Fall back on the old path for compatibility.
+EMU="/run/muos/storage/bios/pico8/pico8_64"
+if [ ! -f "$EMU" ]; then
+	EMU="$EMUDIR/pico8_64"
+fi
+
 chmod +x "$EMUDIR"/wget
-chmod +x "$EMUDIR"/pico8_64
+chmod +x "$EMU"
 
 cd "$EMUDIR" || exit
 
@@ -26,14 +33,14 @@ if [ "$NAME" = "Splore" ]; then
 	$GPTOKEYB "./pico8_64" -c "./pico8.gptk" &
 	PATH="$EMUDIR:$PATH" \
 	HOME="$EMUDIR" \
-	./pico8_64 -windowed 0 -splore
+	"$EMU" -windowed 0 -splore
 else
 	SDL_ASSERT=always_ignore \
 	SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "/usr/lib/gamecontrollerdb.txt") \
 	$GPTOKEYB "./pico8_64" -c "./pico8.gptk" &
 	PATH="$EMUDIR:$PATH" \
 	HOME="$EMUDIR" \
-	./pico8_64 -windowed 0 -run "$ROM"
+	"$EMU" -windowed 0 -run "$ROM"
 fi
 
 kill -9 "$(pidof pico8_64)" "$(pidof gptokeyb2)"

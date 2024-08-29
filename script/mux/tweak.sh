cea3e3a644f52b848c23de39c6cde330edb0d042
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-08-29 19:55:02 +1000

    muOS Internals - Optimised tweak script for general settings

--- script/mux/tweak.sh
+++ script/mux/tweak.sh
@@ -9,32 +9,23 @@ else
 	/opt/muos/device/"$(GET_VAR "device" "board/name")"/input/combo/bright.sh "$C_BRIGHT"
 fi
 
-if [ "$(cat /tmp/mux_colour_temp)" -ne "$(GET_VAR "global" "settings/general/colour")" ]; then
-	GET_VAR "global" "settings/general/colour" >/sys/class/disp/disp/attr/color_temperature
-	GET_VAR "global" "settings/general/colour" >/tmp/mux_colour_temp
-fi
+GET_VAR "global" "settings/general/colour" >/sys/class/disp/disp/attr/color_temperature
 
-if [ "$(cat /tmp/mux_hdmi_mode)" -ne "$(GET_VAR "global" "settings/general/hdmi")" ]; then
-	if [ "$(GET_VAR "global" "settings/general/hdmi")" -gt -1 ]; then
+if [ "$(GET_VAR "global" "settings/general/hdmi")" -gt -1 ]; then
+	killall hdmi_start.sh
+	/opt/muos/device/"$(GET_VAR "device" "board/name")"/script/hdmi_stop.sh
+	if [ "$(GET_VAR "device" "board/hdmi")" -eq 1 ]; then
+		/opt/muos/device/"$(GET_VAR "device" "board/name")"/script/hdmi_start.sh &
+	fi
+else
+	if pgrep -f "hdmi_start.sh" >/dev/null; then
 		killall hdmi_start.sh
 		/opt/muos/device/"$(GET_VAR "device" "board/name")"/script/hdmi_stop.sh
-		if [ "$(GET_VAR "device" "board/hdmi")" -eq 1 ]; then
-			/opt/muos/device/"$(GET_VAR "device" "board/name")"/script/hdmi_start.sh &
-		fi
-	else
-		if pgrep -f "hdmi_start.sh" >/dev/null; then
-			killall hdmi_start.sh
-			/opt/muos/device/"$(GET_VAR "device" "board/name")"/script/hdmi_stop.sh
-		fi
 	fi
-	GET_VAR "global" "settings/general/hdmi" >/tmp/mux_hdmi_mode
 fi
 
-if [ "$(cat /tmp/mux_adb_mode)" -ne "$(GET_VAR "global" "settings/advanced/android")" ]; then
-	if [ "$(GET_VAR "global" "settings/advanced/android")" -eq 1 ]; then
-		/opt/muos/device/"$(GET_VAR "device" "board/name")"/script/adb.sh &
-	else
-		killall -q adbd
-	fi
-	GET_VAR "global" "settings/advanced/android" >/tmp/mux_adb_mode
+if [ "$(GET_VAR "global" "settings/advanced/android")" -eq 1 ]; then
+	/opt/muos/device/"$(GET_VAR "device" "board/name")"/script/adb.sh &
+else
+	killall -q adbd
 fi

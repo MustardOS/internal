d69497179075c1ed70a00076b6fb99b29c3a521b
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-08-30 21:45:24 +1000

    muOS Internals - Fixed low battery indication to stop if charger plugged in

--- script/system/lowpower.sh
+++ script/system/lowpower.sh
@@ -3,12 +3,13 @@
 . /opt/muos/script/var/func.sh
 
 while :; do
+	CHA="$(cat "$(GET_VAR "device" "battery/charger")")"
 	CAP="$(cat "$(GET_VAR "device" "battery/capacity")")"
 	LOW=$(GET_VAR "global" "settings/general/low_battery")
 
 	printf "[BATTERY]\tCAPACITY: %s\tLOW INDICATOR: %s\n" "$CAP" "$LOW"
 
-	if [ "$CAP" -le "$LOW" ]; then
+	if [ "$CAP" -le "$LOW" ] && [ "$CHA" -eq 0 ]; then
 		echo 1 >"$(GET_VAR "device" "led/low")"
 		sleep 0.5
 		echo 0 >"$(GET_VAR "device" "led/low")"

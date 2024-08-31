43365e8a869429ed1901f54b2c25ad9edc9c3a60
Jonathan Rascher <jon@bcat.name>
2024-08-31 17:14:54 -0500

    Stop overwriting RA storage paths on launch

--- script/launch/lr-prboom.sh
+++ script/launch/lr-prboom.sh
@@ -31,22 +31,6 @@ cp -f "$ROMPATH/.IWAD/$IWAD" "$ROMPATH/.$NAME/$IWAD"
 
 RA_CONF="$(GET_VAR "device" "storage/rom/mount")/MUOS/retroarch/retroarch.cfg"
 
-sed -i -e '/^system_directory/d' \
-	-e '/^input_remapping_directory/d' \
-	-e '/^rgui_config_directory/d' \
-	-e '/^savefile_directory/d' \
-	-e '/^savestate_directory/d' \
-	-e '/^screenshot_directory/d' "$RA_CONF"
-
-{
-	echo "system_directory = \"/run/muos/storage/bios\""
-	echo "input_remapping_directory = \"/run/muos/storage/info/config/remaps\""
-	echo "rgui_config_directory = \"/run/muos/storage/info/config\""
-	echo "savefile_directory = \"/run/muos/storage/save/file\""
-	echo "savestate_directory = \"/run/muos/storage/save/state\""
-	echo "screenshot_directory = \"/run/muos/storage/screenshot\""
-} >>"$RA_CONF"
-
 retroarch -v -f -c "$RA_CONF" -L "$(GET_VAR "device" "storage/rom/mount")/MUOS/core/prboom_libretro.so" "$ROMPATH/.$NAME/$IWAD" &
 RA_PID=$!
 
--- script/launch/lr-scummvm.sh
+++ script/launch/lr-scummvm.sh
@@ -28,22 +28,6 @@ cp "$ROMPATH/$NAME.scummvm" "$SCVM"
 
 RA_CONF="$(GET_VAR "device" "storage/rom/mount")/MUOS/retroarch/retroarch.cfg"
 
-sed -i -e '/^system_directory/d' \
-	-e '/^input_remapping_directory/d' \
-	-e '/^rgui_config_directory/d' \
-	-e '/^savefile_directory/d' \
-	-e '/^savestate_directory/d' \
-	-e '/^screenshot_directory/d' "$RA_CONF"
-
-{
-	echo "system_directory = \"/run/muos/storage/bios\""
-	echo "input_remapping_directory = \"/run/muos/storage/info/config/remaps\""
-	echo "rgui_config_directory = \"/run/muos/storage/info/config\""
-	echo "savefile_directory = \"/run/muos/storage/save/file\""
-	echo "savestate_directory = \"/run/muos/storage/save/state\""
-	echo "screenshot_directory = \"/run/muos/storage/screenshot\""
-} >>"$RA_CONF"
-
 retroarch -v -f -c "$RA_CONF" -L "$(GET_VAR "device" "storage/rom/mount")/MUOS/core/scummvm_libretro.so" "$SCVM" &
 RA_PID=$!
 

2b9818a84637675471c2d2165415b980163c43d3
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-09-01 09:48:51 +1000

    muOS Internals - Fixed 32bit external port launcher

--- script/launch/ext-general.sh
+++ script/launch/ext-general.sh
@@ -12,8 +12,13 @@ export SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
 export SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
 export SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
 
+IS_32BIT=0
 if grep -q 'PORT_32BIT="Y"' "$ROM"; then
-	killall -q "golden.sh" "pw-play"
+	IS_32BIT=1
+fi
+
+if [ $IS_32BIT -eq 1 ]; then
+	killall -q "golden.sh" "pw-play" "pipewire" "wireplumber"
 	echo "Switching to ALSA-only configuration..."
 	cp /etc/asound.conf /etc/asound.conf.bak
 	cp /etc/asound.conf.alsa /etc/asound.conf
@@ -32,5 +37,9 @@ if [ -f /etc/asound.conf.bak ]; then
 fi
 
 echo "pipewire" >"$AUDIO_SRC"
+if [ $IS_32BIT -eq 1 ]; then
+	/opt/muos/script/system/pipewire.sh
+fi
+
 amixer -c 0 sset "digital volume" 100%
 /opt/muos/script/mux/golden.sh &

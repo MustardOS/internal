76e00ddeeb3b216949bc9e74742c10a0d3632965
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-09-01 12:34:40 +1000

    muOS Internals - Added event input type to device specific configuration - Fixed Dingux Commander refresh

--- init/MUOS/application/Dingux Commander.sh
+++ init/MUOS/application/Dingux Commander.sh
@@ -16,6 +16,12 @@ cd "$DINGUX_DIR" || exit
 
 SET_VAR "system" "foreground_process" "dingux"
 
+(
+	sleep 1
+	evemu-event "$(GET_VAR "device" "input/ev1")" --type "$(GET_VAR "device" "input/type/dpad/right")" --code "$(GET_VAR "device" "input/code/dpad/right")" --value 1
+	evemu-event "$(GET_VAR "device" "input/ev1")" --type "$(GET_VAR "device" "input/type/dpad/left")" --code "$(GET_VAR "device" "input/code/dpad/left")" --value -1
+) &
+
 SDL_ASSERT=always_ignore SDL_GAMECONTROLLERCONFIG=$(grep "Deeplay" "/usr/lib/gamecontrollerdb.txt") ./dingux --config "$DINGUX_DIR/dingux.cfg"
 
 unset SDL_HQ_SCALER
--- script/var/init/device.sh
+++ script/var/init/device.sh
@@ -25,10 +25,14 @@ BATTERY_VARS="capacity health voltage charger"
 CPU_VARS="cores default governor scaler sampling_rate up_threshold sampling_down_factor io_is_busy sampling_rate_default up_threshold_default sampling_down_factor_default io_is_busy_default"
 BOARD_VARS="name home network bluetooth portmaster lid hdmi event debugfs rtc rumble"
 INPUT_VARS="ev0 ev1 axis_min axis_max"
-INPUT_DPAD_VARS="up down left right"
-INPUT_ANALOG_LEFT_VARS="up down left right click"
-INPUT_ANALOG_RIGHT_VARS="up down left right click"
-INPUT_BUTTON_VARS="a b c x y z l1 l2 l3 r1 r2 r3 menu_short menu_long select start power_short power_long vol_up vol_down"
+INPUT_CODE_DPAD_VARS="up down left right"
+INPUT_CODE_ANALOG_LEFT_VARS="up down left right click"
+INPUT_CODE_ANALOG_RIGHT_VARS="up down left right click"
+INPUT_CODE_BUTTON_VARS="a b c x y z l1 l2 l3 r1 r2 r3 menu_short menu_long select start power_short power_long vol_up vol_down"
+INPUT_TYPE_DPAD_VARS="up down left right"
+INPUT_TYPE_ANALOG_LEFT_VARS="up down left right click"
+INPUT_TYPE_ANALOG_RIGHT_VARS="up down left right click"
+INPUT_TYPE_BUTTON_VARS="a b c x y z l1 l2 l3 r1 r2 r3 menu_short menu_long select start power_short power_long vol_up vol_down"
 LED_VARS="normal low"
 MUX_VARS="width height"
 NETWORK_VARS="module name type iface state"
@@ -40,17 +44,21 @@ STORAGE_ROOT_VARS="active dev sep num mount type"
 STORAGE_SDCARD_VARS="active dev sep num mount type"
 STORAGE_USB_VARS="active dev sep num mount type"
 
-for INIT in audio battery cpu board input input/dpad input/analog/left input/analog/right input/button led mux network screen sdl storage/boot storage/rom storage/root storage/sdcard storage/usb; do
+for INIT in audio battery cpu board input input/code/dpad input/code/analog/left input/code/analog/right input/code/button input/type/dpad input/type/analog/left input/type/analog/right input/type/button led mux network screen sdl storage/boot storage/rom storage/root storage/sdcard storage/usb; do
 	case $INIT in
 		audio) VARS="$AUDIO_VARS" ;;
 		battery) VARS="$BATTERY_VARS" ;;
 		cpu) VARS="$CPU_VARS" ;;
 		board) VARS="$BOARD_VARS" ;;
 		input) VARS="$INPUT_VARS" ;;
-		input/dpad) VARS="$INPUT_DPAD_VARS" ;;
-		input/analog/left) VARS="$INPUT_ANALOG_LEFT_VARS" ;;
-		input/analog/right) VARS="$INPUT_ANALOG_RIGHT_VARS" ;;
-		input/button) VARS="$INPUT_BUTTON_VARS" ;;
+		input/code/dpad) VARS="$INPUT_CODE_DPAD_VARS" ;;
+		input/code/analog/left) VARS="$INPUT_CODE_ANALOG_LEFT_VARS" ;;
+		input/code/analog/right) VARS="$INPUT_CODE_ANALOG_RIGHT_VARS" ;;
+		input/code/button) VARS="$INPUT_CODE_BUTTON_VARS" ;;
+		input/type/dpad) VARS="$INPUT_TYPE_DPAD_VARS" ;;
+		input/type/analog/left) VARS="$INPUT_TYPE_ANALOG_LEFT_VARS" ;;
+		input/type/analog/right) VARS="$INPUT_TYPE_ANALOG_RIGHT_VARS" ;;
+		input/type/button) VARS="$INPUT_TYPE_BUTTON_VARS" ;;
 		led) VARS="$LED_VARS" ;;
 		mux) VARS="$MUX_VARS" ;;
 		network) VARS="$NETWORK_VARS" ;;

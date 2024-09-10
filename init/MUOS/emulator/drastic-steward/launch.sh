d7989da0ee4c1f62c0b005d510b59970804f3d1f
antiKk <antikk@hobbiton.lan>
2024-09-10 13:04:40 +1000

    Replaced drastic-steward with aarch64 build. Thanks @trngaje!

--- init/MUOS/emulator/drastic-steward/launch.sh
+++ init/MUOS/emulator/drastic-steward/launch.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-export CUST_LOGO=0
-export CUST_CPUCLOCK=1
-export USE_752x560_RES=0
 
-LDLIB="$(dirname "$0")/libs":"$LD_LIBRARY_PATH"
+mydir=`dirname "$0"`
 
-export LD_LIBRARY_PATH="$LDLIB"
+
+export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
 export SDL_VIDEODRIVER=mmiyoo
-export SDL_AUDIODRIVER=mmiyoo
 export EGL_VIDEODRIVER=mmiyoo
+
+
+cd $mydir
 
 ./drastic "$1"
 

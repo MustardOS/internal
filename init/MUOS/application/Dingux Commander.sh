2f55800a7c9910cf1adb6de4e2fa40b895b744e3
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-09-25 22:17:21 +1000

    muOS Internals - Fixed Dingux application launch

--- init/MUOS/application/Dingux Commander.sh
+++ init/MUOS/application/Dingux Commander.sh
@@ -22,6 +22,7 @@ SET_VAR "system" "foreground_process" "dingux"
 	while ! pgrep -f "dingux" >/dev/null; do
 		sleep 0.25
 	done
+	sleep 1
 	evemu-event "$(GET_VAR "device" "input/ev1")" --type "$(GET_VAR "device" "input/type/dpad/right")" --code "$(GET_VAR "device" "input/code/dpad/right")" --value 1
 	evemu-event "$(GET_VAR "device" "input/ev1")" --type "$(GET_VAR "device" "input/type/dpad/left")" --code "$(GET_VAR "device" "input/code/dpad/left")" --value -1
 ) &

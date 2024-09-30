af80be91fa40e10f8567afd7a55982e9951e9a72
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-09-30 14:53:04 +1000

    muOS Internals - Fixed small issue with boot/rom partition mount

--- script/mount/mmc.sh
+++ script/mount/mmc.sh
@@ -5,7 +5,7 @@
 DEVICE="$(GET_VAR "device" "storage/rom/dev")$(GET_VAR "device" "storage/rom/sep")$(GET_VAR "device" "storage/rom/num")"
 MOUNT="$(GET_VAR "device" "storage/rom/mount")"
 
-if mount -t "$(GET_VAR "device" "storage/rom/type")" -o rw,utf8,noatime,nofail "$DEVICE" "$MOUNT"; then
+if mount -t "$(GET_VAR "device" "storage/rom/type")" -o rw,utf8,noatime,nofail "/dev/$DEVICE" "$MOUNT"; then
 	SET_VAR "device" "storage/rom/active" "1"
 	SET_VAR "device" "storage/rom/label" "$(blkid -o value -s LABEL "/dev/$DEVICE")"
 fi

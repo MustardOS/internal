3a70830a0817c8009991e9069aabd01287882860
Jonathan Rascher <jon@bcat.name>
2024-09-29 23:28:36 -0500

    Move retroarch.device.cfg out of retroarch mount (#241)

--- init/MUOS/application/RetroArch.sh
+++ init/MUOS/application/RetroArch.sh
@@ -14,4 +14,13 @@ export HOME=$(GET_VAR "device" "board/home")
 
 SET_VAR "system" "foreground_process" "retroarch"
 
-nice --20 /usr/bin/retroarch -v -f -c /run/muos/storage/info/config/retroarch.cfg
+RA_CONF=/run/muos/storage/info/config/retroarch.cfg
+
+# Include default button mappings from retroarch.device.cfg. (Settings in the
+# retroarch.cfg will take precedence. Modified settings will save to the main
+# retroarch.cfg, not the included retroarch.device.cfg.)
+sed -n -e '/^#include /!p' \
+	-e '$a#include "/opt/muos/device/current/control/retroarch.device.cfg"' \
+	-i "$RA_CONF"
+
+nice --20 /usr/bin/retroarch -v -f -c "$RA_CONF"

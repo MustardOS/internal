309071bc5d3b0348f7a4c2d029dce4fec142d19c
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-10-19 21:35:12 +1000

    Update moonlight to use aarch64 gptokeyb

--- init/MUOS/application/Moonlight.sh
+++ init/MUOS/application/Moonlight.sh
@@ -12,7 +12,7 @@ echo app >/tmp/act_go
 
 LOVEDIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/.moonlight"
 MOONDIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/.moonlight/moonlight"
-GPTOKEYB="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/gptokeyb/gptokeyb2.armhf"
+GPTOKEYB="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/gptokeyb/gptokeyb2"
 
 export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
 
@@ -21,7 +21,7 @@ SET_VAR "system" "foreground_process" "love"
 export LD_LIBRARY_PATH="$LOVEDIR/libs:$LD_LIBRARY_PATH"
 $GPTOKEYB "love" &
 ./love gui
-kill -9 "$(pidof gptokeyb2.armhf)"
+kill -9 "$(pidof gptokeyb2)"
 
 cd "$MOONDIR" || exit
 COMMAND=$(cat command.txt)

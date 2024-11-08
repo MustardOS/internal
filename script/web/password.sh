24848a6d0c134f0b4c404fb42d24f31f46893101
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-11-08 14:55:30 +1000

    Adjust formatting

--- script/web/password.sh
+++ script/web/password.sh
@@ -8,15 +8,15 @@ PASS="$(GET_VAR "global" "network/pass")"
 WPA_CONFIG=/etc/wpa_supplicant.conf
 
 case ${#PASS} in
-    64)
-	printf "network={\n\tssid=\"%s\"\n\tpsk=%s\n}" "$SSID" "$PASS" >"$WPA_CONFIG"
-        ;;
-    0)
-	printf "network={\n\tssid=\"%s\"\n\tkey_mgmt=NONE\n}" "$SSID" >"$WPA_CONFIG"
-        ;;
-    *)
-	wpa_passphrase "$SSID" "$PASS" | sed -n '/^[ \t]*psk=/s/^[ \t]*psk=//p' >"/run/muos/global/network/pass"
-	wpa_passphrase "$SSID" "$PASS" >"$WPA_CONFIG"
-	sed -i '3d' "$WPA_CONFIG"
-        ;;
+	64)
+		printf "network={\n\tssid=\"%s\"\n\tpsk=%s\n}" "$SSID" "$PASS" >"$WPA_CONFIG"
+		;;
+	0)
+		printf "network={\n\tssid=\"%s\"\n\tkey_mgmt=NONE\n}" "$SSID" >"$WPA_CONFIG"
+		;;
+	*)
+		wpa_passphrase "$SSID" "$PASS" | sed -n '/^[ \t]*psk=/s/^[ \t]*psk=//p' >"/run/muos/global/network/pass"
+		wpa_passphrase "$SSID" "$PASS" >"$WPA_CONFIG"
+		sed -i '3d' "$WPA_CONFIG"
+		;;
 esac

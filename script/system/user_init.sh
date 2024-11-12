7e52765e2e3db714c8378117a9bda3d1aa576505
Jonathan Rascher <jon@bcat.name>
2024-11-11 22:41:30 -0600

    Run full user init scripts, not just first line

--- script/system/user_init.sh
+++ script/system/user_init.sh
@@ -7,17 +7,14 @@ INIT_LOG="$(GET_VAR "device" "storage/rom/mount")/MUOS/log/user_init.log"
 
 : >"$INIT_LOG"
 
-# Collect and execute the first line of each command file
-for CMD_LINE in "$SCRIPT_DIR"/*.txt; do
-	[ -f "$CMD_LINE" ] || continue
-
-	# Just grab the first line and ignore the rest
-	COMMAND=$(head -n 1 "$CMD_LINE")
+# Collect and execute each script
+for SCRIPT in "$SCRIPT_DIR"/*.sh; do
+	[ -f "$SCRIPT" ] || continue
 
 	# Log it using standard formatting
 	TIME=$(date '+%Y-%m-%d %H:%M:%S')
-	printf "[%s] [${CSI}33m*${ESC}[0m] [%s] %s\n" "$TIME" "$0" "$COMMAND" >>"$INIT_LOG"
+	printf "[%s] [${CSI}33m*${ESC}[0m] [%s] %s\n" "$TIME" "$0" "$SCRIPT" >>"$INIT_LOG"
 
 	# Start the script in the background regardless if it works or not
-	sh -c "$COMMAND" &
+	sh "$SCRIPT" &
 done &

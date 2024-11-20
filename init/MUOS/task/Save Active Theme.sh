12df8f064f2924f5f03c431514741a6e0cbbe2e3
hmcneill46 <harryolivermcneill@gmail.com>
2024-11-20 01:03:56 +0000

    feat: Call resulting theme the name of the theme it came from

--- init/MUOS/task/Save Active Theme.sh
+++ init/MUOS/task/Save Active Theme.sh
@@ -11,8 +11,20 @@ DEST_DIR="/run/muos/storage/theme"
 # Generate the timestamp for the backup filename
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
+# Check if theme_name.txt exists and read the base theme name
+if [ -f "$SOURCE_DIR/theme_name.txt" ]; then
+    THEME_NAME_FILE="theme_name.txt"
+    BASE_THEME_NAME=$(sed -n '1p' "$SOURCE_DIR/$THEME_NAME_FILE")
+    echo "$BASE_THEME_NAME is the base theme name"
+else
+    BASE_THEME_NAME="active"
+    echo "theme_name.txt not found. Using default theme name: $BASE_THEME_NAME"
+fi
+
+echo "$BASE_THEME_NAME is the base theme name"
+
 # Construct the destination file name
-DEST_FILE="$DEST_DIR/active-$TIMESTAMP.zip"
+DEST_FILE="$DEST_DIR/$BASE_THEME_NAME-$TIMESTAMP.zip"
 
 # Check if the source directory exists
 if [ ! -d "$SOURCE_DIR" ]; then

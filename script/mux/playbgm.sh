77c0e31ab762677a9b159a76c4b907b35acbf8f7
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-11-25 20:31:16 +1000

    Adjusted BGM script to play any valid audio file via mpv

--- script/mux/playbgm.sh
+++ script/mux/playbgm.sh
@@ -2,10 +2,26 @@
 
 . /opt/muos/script/var/func.sh
 
-MP3_DIR="/run/muos/storage/music"
+BGM_GLOBAL_DIR="/run/muos/storage/music"
+BGM_THEME_DIR="/run/muos/storage/theme/active/music"
+
+BGM_TYPE=$(GET_VAR "global" "settings/general/bgm")
+
+PLAY_RANDOM_BGM() {
+	DIR="$1"
+	if [ -d "$DIR" ]; then
+		find "$DIR" -maxdepth 1 -type f | shuf -n 1 | while IFS= read -r BGM_FILE; do
+			mpv "$BGM_FILE"
+		done
+	fi
+}
 
 while :; do
-	MP3_FILES=$(find "$MP3_DIR" -maxdepth 1 -type f -name "*.mp3")
-	[ -n "$MP3_FILES" ] && mpg123 -Z "$MP3_DIR"/*.mp3
+	case $BGM_TYPE in
+		1) PLAY_RANDOM_BGM "$BGM_GLOBAL_DIR" ;;
+		2) PLAY_RANDOM_BGM "$BGM_THEME_DIR" ;;
+	esac
+
+	# Have a wee nap...
 	sleep 1
 done &

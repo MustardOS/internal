8f5d594d3fc171973494944bc83ca00d38b2e5d6
antiKk <antikk@hobbiton.lan>
2024-11-25 19:20:04 +1000

    Added per-game save files-ish

--- init/MUOS/task/Restore YabaSanshiro Configuration.sh
+++ init/MUOS/task/Restore YabaSanshiro Configuration.sh
@@ -9,14 +9,14 @@ pkill -STOP muxtask
 MOUNT="$(GET_VAR device storage/rom/mount)"
 
 # Restore control config
-echo "Restoring YabaSanshiro Configuration"
+echo "Restoring YabaSanshiro Controls"
 rm -f "$MOUNT/MUOS/emulator/yabasanshiro/.emulationstation/es_temporaryinput.cfg" \
 	"$MOUNT/MUOS/emulator/yabasanshiro/.yabasanshiro/keymapv2.json"
 /opt/muos/device/current/script/control.sh
 
 # Remove any per-game configs
 CONF_DIR="$MOUNT/MUOS/emulator/yabasanshiro/.yabasanshiro"
-echo "Removing per game configs"
+echo "Removing Per-game YabaSanshiro Configurations"
 for file in "$CONF_DIR"/*.config; do
     if [ "$(basename "$file")" != "28xx.config" ]; then
         rm -f "$file"

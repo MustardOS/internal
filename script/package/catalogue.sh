23dc27924317f959b84f3a95b899f668cdd104ca
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-11-25 19:48:08 +1000

    Change package content deletion logic

--- script/package/catalogue.sh
+++ script/package/catalogue.sh
@@ -5,20 +5,21 @@
 CATALOGUE_DIR="/run/muos/storage/info/catalogue"
 CATALOGUE_ZIP="/run/muos/storage/package/catalogue/$1.zip"
 
-while [ -d "$CATALOGUE_DIR" ]; do
-	[ -z "$(ls -A "$CATALOGUE_DIR" 2>/dev/null)" ] && break
-	rm -rf "$CATALOGUE_DIR/*"
+# Ensure the catalogue directory is empty without deleting the directory itself
+if [ -d "$CATALOGUE_DIR" ]; then
+	printf "Purging catalogue directory: %s" "$CONFIG_DIR"
+	find "$CATALOGUE_DIR" -mindepth 1 -exec rm -rf {} + 2>/dev/null
 	sync
-	sleep 1
-done
+fi
 
 unzip "$CATALOGUE_ZIP" -d "$CATALOGUE_DIR"
 
 # Run the catalogue generation script just in case people have forgotten things...
+printf "Running catalogue generation script"
 /opt/muos/script/system/catalogue.sh
 
-$CATALOGUE_NAME=$(basename "$CATALOGUE_ZIP" .zip)
-CLEANED_CATALOGUE_NAME=$(echo "$CATALOGUE_NAME" | sed -E 's/-[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}$//')
+CATALOGUE_NAME=$(basename "$CATALOGUE_ZIP" .zip)
+CLEANED_CATALOGUE_NAME=$(printf "%s\n" "$CATALOGUE_NAME" | sed -E 's/-[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}$//')
 echo "$CLEANED_CATALOGUE_NAME" >"$CATALOGUE_DIR/catalogue_name.txt"
 
 sync
--- script/package/config.sh
+++ script/package/config.sh
@@ -5,20 +5,21 @@
 CONFIG_DIR="/run/muos/storage/info/config"
 CONFIG_ZIP="/run/muos/storage/package/config/$1.zip"
 
-while [ -d "$CONFIG_DIR" ]; do
-	[ -z "$(ls -A "$CONFIG_DIR" 2>/dev/null)" ] && break
-	rm -rf "$CONFIG_DIR/*"
+# Ensure the configuration directory is empty without deleting the directory itself
+if [ -d "$CONFIG_DIR" ]; then
+	printf "Purging configuration directory: %s" "$CONFIG_DIR"
+	find "$CONFIG_DIR" -mindepth 1 -exec rm -rf {} + 2>/dev/null
 	sync
-	sleep 1
-done
+fi
 
 unzip "$CONFIG_ZIP" -d "$CONFIG_DIR"
 
 # Run the device control configuration just in case people have forgotten about the retroarch.cfg file...
+printf "Restoring device control configuration"
 /opt/muos/device/current/script/control.sh
 
 CONFIG_NAME=$(basename "$CONFIG_ZIP" .zip)
-CLEANED_CONFIG_NAME=$(echo "$CONFIG_NAME" | sed -E 's/-[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}$//')
+CLEANED_CONFIG_NAME=$(printf "%s\n" "$CONFIG_NAME" | sed -E 's/-[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}$//')
 echo "$CLEANED_CONFIG_NAME" >"$CONFIG_DIR/config_name.txt"
 
 sync

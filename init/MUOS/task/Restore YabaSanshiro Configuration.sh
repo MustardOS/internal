c0f5a3742ab2248707523f286f274d73b4c76b15
antiKk <4548266+antiKk@users.noreply.github.com>
2024-11-25 12:45:13 +1000

    Fix YabaSanshiro 28xx rotation. (#395)
    Co-authored-by: antiKk <antikk@hobbiton.lan>

--- init/MUOS/task/Restore YabaSanshiro Configuration.sh
+++ init/MUOS/task/Restore YabaSanshiro Configuration.sh
@@ -8,10 +8,20 @@ pkill -STOP muxtask
 
 MOUNT="$(GET_VAR device storage/rom/mount)"
 
+# Restore control config
 echo "Restoring YabaSanshiro Configuration"
 rm -f "$MOUNT/MUOS/emulator/yabasanshiro/.emulationstation/es_temporaryinput.cfg" \
 	"$MOUNT/MUOS/emulator/yabasanshiro/.yabasanshiro/keymapv2.json"
 /opt/muos/device/current/script/control.sh
+
+# Remove any per-game configs
+CONF_DIR="$MOUNT/MUOS/emulator/yabasanshiro/.yabasanshiro"
+echo "Removing per game configs"
+for file in "$CONF_DIR"/*.config; do
+    if [ "$(basename "$file")" != "28xx.config" ]; then
+        rm -f "$file"
+    fi
+done
 
 echo "Sync Filesystem"
 sync
--- script/launch/ext-yabasanshiro.sh
+++ script/launch/ext-yabasanshiro.sh
@@ -5,10 +5,18 @@
 NAME=$1
 CORE=$2
 ROM=$3
-BIOS="/run/muos/storage/bios/saturn_bios.bin"
 
-mkdir -p /run/muos/storage/save/file/YabaSanshiro-Ext
-mkdir -p /run/muos/storage/save/state/YabaSanshiro-Ext
+BIOS="/run/muos/storage/bios/saturn_bios.bin"
+SAVE_DIR="/run/muos/storage/save/file/YabaSanshiro-Ext"
+STATE_DIR="/run/muos/storage/save/state/YabaSanshiro-Ext"
+
+# Create save directories if absent
+if [ ! -d "$SAVE_DIR" ]; then
+	mkdir -p "$SAVE_DIR"
+fi
+if [ ! -d "$STATE_DIR" ]; then
+	mkdir -p "$STATE_DIR"
+fi
 
 if [ "$CORE" = "ext-yabasanshiro-hle" ]; then
 	YABA_BIN="./yabasanshiro -r 3 -a -i"
@@ -18,14 +26,28 @@ elif [ "$CORE" = "ext-yabasanshiro-bios" ] ; then
 	YABA_BIN="./yabasanshiro -b $BIOS -r 3 -a -i"
 fi
 
+CURR_CONSOLE="$(GET_VAR "device" "board/name")"
+
 export SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
 export SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
-export SDL_BLITTER_DISABLED=1
+export SDL_BLITTER_DISABLED=0
 
 SET_VAR "system" "foreground_process" "yabasanshiro"
 
 EMUDIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/yabasanshiro"
 export HOME="$EMUDIR"
+
+CONF_28XX="$EMUDIR/.yabasanshiro/28xx.config"
+
+# Grab full ROM name including extension
+ROMNAME=$(basename "$ROM")
+
+# YabaSanshiro appears to rotate on a game config level.
+# This copies a rotation enabled game config as game specific before launch.
+# NOTE: Menu rotation is currently still wrong.
+if [ "$CURR_CONSOLE" = "rg28xx" ] && [ ! -f "$EMUDIR/.yabasanshiro/$ROMNAME.config" ]; then
+    cp -f "$CONF_28XX" "$EMUDIR/.yabasanshiro/$ROMNAME.config"
+fi
 
 chmod +x "$EMUDIR"/yabasanshiro
 

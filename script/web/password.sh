e60a35f2471be44d3ffd5aeac88626cb3d51581c
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-12-01 11:52:47 +1000

    Added hidden SSID network connection support

--- script/web/password.sh
+++ script/web/password.sh
@@ -3,16 +3,17 @@
 . /opt/muos/script/var/func.sh
 
 SSID="$(GET_VAR "global" "network/ssid")"
+SCAN="$(GET_VAR "global" "network/scan")"
 PASS="$(GET_VAR "global" "network/pass")"
 
 WPA_CONFIG=/etc/wpa_supplicant.conf
 
 case ${#PASS} in
 	64)
-		printf "network={\n\tssid=\"%s\"\n\tpsk=%s\n}" "$SSID" "$PASS" >"$WPA_CONFIG"
+		printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tpsk=%s\n}" "$SSID" "$SCAN" "$PASS" >"$WPA_CONFIG"
 		;;
 	0)
-		printf "network={\n\tssid=\"%s\"\n\tkey_mgmt=NONE\n}" "$SSID" >"$WPA_CONFIG"
+		printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tkey_mgmt=NONE\n}" "$SSID" "$SCAN" >"$WPA_CONFIG"
 		;;
 	*)
 		wpa_passphrase "$SSID" "$PASS" | sed -n '/^[ \t]*psk=/s/^[ \t]*psk=//p' >"/run/muos/global/network/pass"

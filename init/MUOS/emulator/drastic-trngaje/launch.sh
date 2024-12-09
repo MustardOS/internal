77c21d55a4d4e384539c05dcdb0b28980b6151c2
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-12-09 19:37:58 +1000

    Fixed drastic launch helper script

--- init/MUOS/emulator/drastic-trngaje/launch.sh
+++ init/MUOS/emulator/drastic-trngaje/launch.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-mydir=`dirname "$0"`
+DRASTIC_DIR=$(dirname "$0")
+export LD_LIBRARY_PATH=$DRASTIC_DIR/libs:$LD_LIBRARY_PATH
 
-
-export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
-#export SDL_VIDEODRIVER=mmiyoo
-#export EGL_VIDEODRIVER=mmiyoo
-
-
-cd $mydir
-
+cd "$DRASTIC_DIR" || exit 1
 ./drastic "$1"
 
+U_DATA="/userdata"
+[ -d "$U_DATA" ] && rm -rf "$U_DATA"

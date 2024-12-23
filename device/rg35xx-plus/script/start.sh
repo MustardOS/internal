9c540b8285df5de7601466366c2f7a085b47fb8f
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-12-23 13:58:03 +1000

    Moved GPU frequency setting from device startup to module startup to ensure Mali is initiated first

--- device/rg35xx-plus/script/start.sh
+++ device/rg35xx-plus/script/start.sh
@@ -44,8 +44,3 @@ if [ "$(GET_VAR "global" "settings/advanced/thermal")" -eq 1 ]; then
 		fi
 	done
 fi
-
-# Switch GPU power policy and set to maximum frequency
-echo always_on >/sys/devices/platform/gpu/power_policy &
-echo 648000000 >/sys/devices/platform/gpu/devfreq/gpu/min_freq &
-echo 648000000 >/sys/devices/platform/gpu/devfreq/gpu/max_freq &

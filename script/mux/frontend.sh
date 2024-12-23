2e3405efa4d3ed596a3b8d7668b1a2d3b687ddac
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2024-12-23 17:10:43 +1000

    Fixed RetroArch network wait status messages

--- script/mux/frontend.sh
+++ script/mux/frontend.sh
@@ -69,32 +69,32 @@ if [ "$(GET_VAR "global" "settings/general/startup")" = "last" ] || [ "$(GET_VAR
 			OIP=0
 			while :; do
 				NW_MSG=$(printf "Waiting for network to connect... (%s)\n\nPress START to continue loading\nPress SELECT to go to main menu" "$OIP")
-				/opt/muos/extra/muxstart "$NW_MSG"
+				/opt/muos/extra/muxstart 0 "$NW_MSG"
 				OIP=$((OIP + 1))
 				if [ "$(cat "$(GET_VAR "device" "network/state")")" = "up" ]; then
 					LOG_SUCCESS "$0" 0 "FRONTEND" "Network connected"
-					/opt/muos/extra/muxstart "Network connected"
+					/opt/muos/extra/muxstart 0 "Network connected"
 
 					PIP=0
 					while ! ping -c 1 -W 5 8.8.8.8 >/dev/null 2>&1; do
 						PIP=$((PIP + 1))
 						LOG_INFO "$0" 0 "FRONTEND" "Verifying connectivity..."
-						/opt/muos/extra/muxstart "Verifying connectivity... (%s)" "$PIP"
+						/opt/muos/extra/muxstart 0 "Verifying connectivity... (%s)" "$PIP"
 						sleep 1
 					done
 
 					LOG_SUCCESS "$0" 0 "FRONTEND" "Connectivity verified!"
-					/opt/muos/extra/muxstart "Connectivity verified! Booting content!"
+					/opt/muos/extra/muxstart 0 "Connectivity verified! Booting content!"
 					break
 				fi
 				if [ "$(cat "$NET_START")" = "ignore" ]; then
 					LOG_SUCCESS "$0" 0 "FRONTEND" "Ignoring network connection"
-					/opt/muos/extra/muxstart "Ignoring network connection... Booting content!"
+					/opt/muos/extra/muxstart 0 "Ignoring network connection... Booting content!"
 					break
 				fi
 				if [ "$(cat "$NET_START")" = "menu" ]; then
 					LOG_SUCCESS "$0" 0 "FRONTEND" "Booting to main menu"
-					/opt/muos/extra/muxstart "Booting to main menu!"
+					/opt/muos/extra/muxstart 0 "Booting to main menu!"
 					break
 				fi
 				sleep 1

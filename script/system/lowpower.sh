9fe07edd05505323525eb436442671121fd3861a
kubrick89 <83338016+kubrick89@users.noreply.github.com>
2025-01-18 16:27:40 +0100

    Use RGB as low battery indicator (#450)
    * Use RGB as low battery indicator
    
    * Moved sleep in lowpower.sh outside of if

--- script/system/lowpower.sh
+++ script/system/lowpower.sh
@@ -5,11 +5,17 @@
 while :; do
 	if [ "$(cat "$(GET_VAR "device" "battery/charger")")" -eq 0 ]; then
 		if [ "$(cat "$(GET_VAR "device" "battery/capacity")")" -le "$(GET_VAR "global" "settings/power/low_battery")" ]; then
+			if [ "$(GET_VAR device led/rgb)" -eq 1 ]; then
+				/opt/muos/device/current/script/led_control.sh 2 255 255 0 0
+			fi					
 			echo 1 >"$(GET_VAR "device" "led/low")"
 			sleep 0.5
 			echo 0 >"$(GET_VAR "device" "led/low")"
+		else
+			if [ "$(GET_VAR device led/rgb)" -eq 1 ]; then
+				/run/muos/storage/theme/active/rgb/rgbconf.sh
+			fi
 		fi
 	fi
-
-	sleep 10
+ 	sleep 10
 done &

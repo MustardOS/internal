66308a2e1edebfb8926ba56ba1be5fa9486d7cd4
fishcu <isaacbd@gmail.com>
2025-01-18 07:27:11 +0100

    Update GLSL shaders from upstream commit a1d856bf (#447)

--- init/MUOS/retroarch/shaders/pixel-art-scaling/shaders/pixel_aa/pixel_aa.glsl
+++ init/MUOS/retroarch/shaders/pixel-art-scaling/shaders/pixel_aa/pixel_aa.glsl
@@ -2,7 +2,7 @@
 
 /*
     Pixel AA by fishku
-    Copyright (C) 2023-2024
+    Copyright (C) 2023-2025
     Public domain license (CC0)
 
     Features:
@@ -24,6 +24,8 @@
     subpixel anti-aliasing, results are identical to the "pixellate" shader.
 
     Changelog:
+    v1.10: Fix bug with precision specifiers causing trouble on certain compilers.
+    v1.9: Sync GLSL version with Slang version 1.9.
     v1.7: Clean up. Optimize through precision specifiers and separate
           linearization.
     v1.6: Add "fast" version for low-end devices.
@@ -38,7 +40,7 @@
 */
 
 // clang-format off
-#pragma parameter PIX_AA_SETTINGS "=== Pixel AA v1.7 settings ===" 0.0 0.0 1.0 1.0
+#pragma parameter PIX_AA_SETTINGS "=== Pixel AA v1.10 settings ===" 0.0 0.0 1.0 1.0
 #pragma parameter PIX_AA_SHARP "Pixel AA sharpening amount" 1.5 0.0 2.0 0.05
 #pragma parameter PIX_AA_SUBPX "Enable subpixel AA" 0.0 0.0 1.0 1.0
 #pragma parameter PIX_AA_SUBPX_ORIENTATION "Subpixel layout (0=RGB, 1=RGB vert., 2=BGR, 3=BGR vert.)" 0.0 0.0 3.0 1.0
@@ -56,23 +58,52 @@
 
 #if defined(VERTEX)
 
-uniform PREC_MED mat4 MVPMatrix;
-uniform PREC_MED vec2 OutputSize;
-uniform PREC_MED vec2 TextureSize;
-uniform PREC_MED vec2 InputSize;
+uniform PREC_HIGH mat4 MVPMatrix;
+uniform PREC_HIGH vec2 TextureSize;
+uniform PREC_HIGH vec2 InputSize;
+uniform PREC_HIGH vec2 OutputSize;
+uniform PREC_LOW int Rotation;
 
-in PREC_MED vec4 VertexCoord;
-in PREC_MED vec4 TexCoord;
+uniform PREC_MED float PIX_AA_SHARP;
+uniform PREC_MED float PIX_AA_SUBPX_ORIENTATION;
 
-out PREC_MED vec2 tx_coord;
-out PREC_MED vec2 tx_per_px;
+in PREC_HIGH vec4 VertexCoord;
+in PREC_HIGH vec4 TexCoord;
+
+out PREC_HIGH vec2 tx_coord;
 out PREC_MED vec2 tx_to_uv;
+out PREC_MED vec2 trans_lb;
+out PREC_MED vec2 trans_ub;
+out PREC_MED vec2 sub_tx_offset;
+out PREC_MED float trans_slope;
+
+void calculate_pixel_aa_params(PREC_MED vec2 tx_per_px, PREC_MED float sharpness,
+                               PREC_LOW int subpx_orientation, PREC_LOW int rotation,
+                               inout PREC_MED vec2 trans_lb, inout PREC_MED vec2 trans_ub,
+                               inout PREC_MED float trans_slope,
+                               inout PREC_MED vec2 sub_tx_offset) {
+    PREC_MED float sharpness_upper = min(1.0, sharpness);
+    trans_lb = sharpness_upper * (0.5 - 0.5 * tx_per_px);
+    trans_ub = 1.0 - sharpness_upper * (1.0 - (0.5 + 0.5 * tx_per_px));
+    trans_slope = max(1.0, sharpness);
+
+    // Subpixel sampling: Shift the sampling by 1/3rd of an output pixel for
+    // each subpixel, assuming that the output size is at monitor
+    // resolution.
+    // Compensate for possible rotation of the screen in certain cores.
+    const PREC_MED vec4 rot_corr = vec4(1.0, 0.0, -1.0, 0.0);
+    sub_tx_offset = tx_per_px / 3.0 *
+                    vec2(rot_corr[(rotation + subpx_orientation) % 4],
+                         rot_corr[(rotation + subpx_orientation + 3) % 4]);
+}
 
 void main() {
     gl_Position = MVPMatrix * VertexCoord;
     tx_coord = TexCoord.xy * TextureSize;
-    tx_per_px = InputSize / OutputSize;
     tx_to_uv = 1.0 / TextureSize;
+
+    calculate_pixel_aa_params(InputSize / OutputSize, PIX_AA_SHARP, int(PIX_AA_SUBPX_ORIENTATION),
+                              Rotation, trans_lb, trans_ub, trans_slope, sub_tx_offset);
 }
 
 #elif defined(FRAGMENT)
@@ -85,17 +116,16 @@ precision mediump float;
 #endif
 #endif
 
-uniform PREC_LOW int Rotation;
 uniform PREC_LOW sampler2D Texture;
 
-uniform PREC_MED float PIX_AA_SHARP;
-uniform PREC_LOW float PIX_AA_GAMMA;
 uniform PREC_LOW float PIX_AA_SUBPX;
-uniform PREC_MED float PIX_AA_SUBPX_ORIENTATION;
 
-in PREC_MED vec2 tx_coord;
-in PREC_MED vec2 tx_per_px;
+in PREC_HIGH vec2 tx_coord;
 in PREC_MED vec2 tx_to_uv;
+in PREC_MED vec2 trans_lb;
+in PREC_MED vec2 trans_ub;
+in PREC_MED vec2 sub_tx_offset;
+in PREC_MED float trans_slope;
 
 out PREC_LOW vec4 FragColor;
 
@@ -109,71 +139,48 @@ PREC_MED vec2 slopestep(PREC_MED vec2 edge0, PREC_MED vec2 edge1, PREC_MED vec2 
     return o - 0.5 * s * pow(2.0 * (o - s * x), vec2(slope));
 }
 
-// Function to get a pixel value, taking into consideration possible subpixel
-// interpolation.
-PREC_LOW vec3 pixel_aa(PREC_LOW sampler2D tex, PREC_MED vec2 tx_per_px, PREC_MED vec2 tx_to_uv,
-                       PREC_MED vec2 tx_coord, PREC_MED float sharpness, bool sample_subpx,
-                       PREC_LOW int subpx_orientation, PREC_LOW int screen_rotation) {
-    PREC_MED float sharpness_upper = min(1.0, sharpness);
-    PREC_MED vec2 sharp_lb = sharpness_upper * (0.5 - 0.5 * tx_per_px);
-    PREC_MED vec2 sharp_ub = 1.0 - sharpness_upper * (1.0 - (0.5 + 0.5 * tx_per_px));
-    PREC_MED float sharpness_lower = max(1.0, sharpness);
+PREC_LOW vec3 pixel_aa(PREC_LOW sampler2D tex, PREC_HIGH vec2 tx_coord, PREC_MED vec2 tx_to_uv,
+                       PREC_MED vec2 trans_lb, PREC_MED vec2 trans_ub, PREC_MED float trans_slope) {
+    PREC_MED vec2 period = floor(tx_coord - 0.5);
+    PREC_MED vec2 phase = tx_coord - 0.5 - period;
 
+    PREC_MED vec2 offset = slopestep(trans_lb, trans_ub, phase, trans_slope);
+
+    return texture(tex, (period + 0.5 + offset) * tx_to_uv).rgb;
+}
+
+PREC_LOW vec3 pixel_aa_subpx(PREC_LOW sampler2D tex, PREC_HIGH vec2 tx_coord,
+                             PREC_MED vec2 sub_tx_offset, PREC_MED vec2 tx_to_uv,
+                             PREC_MED vec2 trans_lb, PREC_MED vec2 trans_ub,
+                             PREC_MED float trans_slope) {
+    PREC_LOW vec3 res;
     PREC_MED vec2 period, phase, offset;
-    if (sample_subpx) {
-        // Subpixel sampling: Shift the sampling by 1/3rd of an output pixel for
-        // each subpixel, assuming that the output size is at monitor
-        // resolution.
-        // Compensate for possible rotation of the screen in certain cores.
-        const vec4 rot_corr = vec4(1.0, 0.0, -1.0, 0.0);
-        PREC_MED vec2 sub_tx_offset = tx_per_px / 3.0 *
-                                      vec2(rot_corr[(screen_rotation + subpx_orientation) % 4],
-                                           rot_corr[(screen_rotation + subpx_orientation + 3) % 4]);
 
-        PREC_LOW vec3 res;
+    // Red
+    period = floor(tx_coord - sub_tx_offset - 0.5);
+    phase = tx_coord - sub_tx_offset - 0.5 - period;
+    offset = slopestep(trans_lb, trans_ub, phase, trans_slope);
+    res.r = texture(tex, (period + 0.5 + offset) * tx_to_uv).r;
+    // Green
+    period = floor(tx_coord - 0.5);
+    phase = tx_coord - 0.5 - period;
+    offset = slopestep(trans_lb, trans_ub, phase, trans_slope);
+    res.g = texture(tex, (period + 0.5 + offset) * tx_to_uv).g;
+    // Blue
+    period = floor(tx_coord + sub_tx_offset - 0.5);
+    phase = tx_coord + sub_tx_offset - 0.5 - period;
+    offset = slopestep(trans_lb, trans_ub, phase, trans_slope);
+    res.b = texture(tex, (period + 0.5 + offset) * tx_to_uv).b;
 
-        // Red
-        period = floor(tx_coord - sub_tx_offset - 0.5);
-        phase = tx_coord - sub_tx_offset - 0.5 - period;
-        offset = slopestep(sharp_lb, sharp_ub, phase, sharpness_lower);
-        res.r = texture(tex, (period + 0.5 + offset) * tx_to_uv).r;
-        // Green
-        period = floor(tx_coord - 0.5);
-        phase = tx_coord - 0.5 - period;
-        offset = slopestep(sharp_lb, sharp_ub, phase, sharpness_lower);
-        res.g = texture(tex, (period + 0.5 + offset) * tx_to_uv).g;
-        // Blue
-        period = floor(tx_coord + sub_tx_offset - 0.5);
-        phase = tx_coord + sub_tx_offset - 0.5 - period;
-        offset = slopestep(sharp_lb, sharp_ub, phase, sharpness_lower);
-        res.b = texture(tex, (period + 0.5 + offset) * tx_to_uv).b;
-
-        return res;
-    } else {
-        // The offset for interpolation is a periodic function with
-        // a period length of 1 texel.
-        // The input coordinate is shifted so that the center of the texel
-        // aligns with the start of the period.
-        // First, get the period and phase.
-        period = floor(tx_coord - 0.5);
-        phase = tx_coord - 0.5 - period;
-        // The function starts at 0, then starts transitioning at
-        // 0.5 - 0.5 / pixels_per_texel, then reaches 0.5 at 0.5,
-        // Then reaches 1 at 0.5 + 0.5 / pixels_per_texel.
-        // For sharpness values < 1.0, blend to bilinear filtering.
-        offset = slopestep(sharp_lb, sharp_ub, phase, sharpness_lower);
-
-        // When the input is in linear color space, we can make use of a single tap
-        // using bilinear interpolation. The offsets are shifted back to the texel
-        // center before sampling.
-        return texture(tex, (period + 0.5 + offset) * tx_to_uv).rgb;
-    }
+    return res;
 }
 
 void main() {
-    FragColor.rgb = pow(pixel_aa(Texture, tx_per_px, tx_to_uv, tx_coord, PIX_AA_SHARP,
-                                 PIX_AA_SUBPX > 0.5, int(PIX_AA_SUBPX_ORIENTATION), Rotation),
-                        vec3(1.0 / 2.2));
+    FragColor.rgb = PIX_AA_SUBPX < 0.5
+                        ? pixel_aa(Texture, tx_coord, tx_to_uv, trans_lb, trans_ub, trans_slope)
+                        : pixel_aa_subpx(Texture, tx_coord, sub_tx_offset, tx_to_uv, trans_lb,
+                                         trans_ub, trans_slope);
+    FragColor.rgb = pow(FragColor.rgb, vec3(1.0 / 2.2));
 }
 
 #endif

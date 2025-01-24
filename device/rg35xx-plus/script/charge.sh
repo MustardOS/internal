6fc319c1256b2f584e9893bbf73b49c656fc2281
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-01-24 21:25:15 +1000

    Fixed charging scripts due to SDL2 change to charging module

--- device/rg35xx-plus/script/charge.sh
+++ device/rg35xx-plus/script/charge.sh
@@ -2,19 +2,23 @@
 
 . /opt/muos/script/var/func.sh
 
-if [ "$(cat "$(GET_VAR "device" "battery/boot_mode")")" -eq 1 ] && [ "$(GET_VAR "global" "boot/factory_reset")" -eq 0 ]; then
-	SET_VAR "system" "foreground_process" "muxcharge"
+BOOT_MODE=$(GET_VAR "device" "battery/boot_mode")
+FACTORY_RESET=$(GET_VAR "global" "boot/factory_reset")
+DEBUGFS=$(GET_VAR "device" "board/debugfs")
+GOVERNOR=$(GET_VAR "device" "cpu/governor")
+LED=$(GET_VAR "device" "led/normal")
+EXIT_STATUS="/tmp/charger_exit"
 
-	if [ "$(GET_VAR "device" "board/debugfs")" -eq 1 ]; then
-		mount -t debugfs debugfs /sys/kernel/debug
-	fi
+if read -r MODE <"$BOOT_MODE" && [ "$MODE" -eq 1 ] && [ "$FACTORY_RESET" -eq 0 ]; then
+	[ "$DEBUGFS" -eq 1 ] && mount -t debugfs debugfs /sys/kernel/debug
 
-	echo "powersave" >"$(GET_VAR "device" "cpu/governor")"
+	echo "powersave" >"$GOVERNOR"
+	EXEC_MUX "" "muxcharge"
 
-	if ! /opt/muos/extra/muxcharge; then
+	if read -r CHARGER_EXIT <$EXIT_STATUS && [ "$CHARGER_EXIT" -eq 1 ]; then
 		/opt/muos/script/system/halt.sh poweroff
 	fi
 
-	echo "performance" >"$(GET_VAR "device" "cpu/governor")"
-	echo 1 >"$(GET_VAR "device" "led/normal")"
+	echo "performance" >"$GOVERNOR"
+	echo 1 >"$LED"
 fi

0a2dfca4c069d371674869d55904151b29df6a43
tr12345 <27497732+GrumpyGopher@users.noreply.github.com>
2025-02-24 20:20:15 -0500

    fix device booting after unplugging charging cable

--- device/brick/script/charge.sh
+++ device/brick/script/charge.sh
@@ -7,7 +7,7 @@ FACTORY_RESET=$(GET_VAR "global" "boot/factory_reset")
 DEBUGFS=$(GET_VAR "device" "board/debugfs")
 GOVERNOR=$(GET_VAR "device" "cpu/governor")
 LED=$(GET_VAR "device" "led/normal")
-EXIT_STATUS="/tmp/charger_exit"
+EXIT_STATUS_FILE="/tmp/charger_exit"
 
 if read -r MODE <"$BOOT_MODE" && [ "$MODE" -eq 1 ] && [ "$FACTORY_RESET" -eq 0 ]; then
 	[ "$DEBUGFS" -eq 1 ] && mount -t debugfs debugfs /sys/kernel/debug
@@ -15,7 +15,7 @@ if read -r MODE <"$BOOT_MODE" && [ "$MODE" -eq 1 ] && [ "$FACTORY_RESET" -eq 0 ]
 	echo "powersave" >"$GOVERNOR"
 	EXEC_MUX "" "muxcharge"
 
-	if read -r CHARGER_EXIT <$EXIT_STATUS && [ "$CHARGER_EXIT" -eq 1 ]; then
+	if read -r CHARGER_EXIT <$EXIT_STATUS_FILE && [ "$CHARGER_EXIT" -eq 1 ]; then
 		/opt/muos/script/system/halt.sh poweroff
 	fi
 

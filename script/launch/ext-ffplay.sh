8ed7410110f6420109d390b132cc892c559de6ca
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-03-09 13:30:50 +1000

    Adjusted launch scripts to take advantage of Console Mode

--- script/launch/ext-ffplay.sh
+++ script/launch/ext-ffplay.sh
@@ -14,9 +14,16 @@ LOG_INFO "$0" 0 "FILE" "$FILE"
 HOME="$(GET_VAR "device" "board/home")"
 export HOME
 
-SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
-SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
-SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
+if [ "$(cat "$(GET_VAR "device" "screen/hdmi")")" -eq 1 ] && [ "$(GET_VAR "device" "board/hdmi")" -eq 1 ]; then
+	SDL_HQ_SCALER=2
+	SDL_ROTATION=0
+	SDL_BLITTER_DISABLED=1
+else
+	SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
+	SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
+	SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
+fi
+
 export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
 
 SET_VAR "system" "foreground_process" "ffplay"
--- script/launch/ext-ppsspp.sh
+++ script/launch/ext-ppsspp.sh
@@ -15,9 +15,16 @@ PPSSPP_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/ppsspp"
 HOME="$PPSSPP_DIR"
 export HOME
 
-SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
-SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
-SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
+if [ "$(cat "$(GET_VAR "device" "screen/hdmi")")" -eq 1 ] && [ "$(GET_VAR "device" "board/hdmi")" -eq 1 ]; then
+	SDL_HQ_SCALER=2
+	SDL_ROTATION=0
+	SDL_BLITTER_DISABLED=1
+else
+	SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
+	SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
+	SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
+fi
+
 export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
 
 cd "$PPSSPP_DIR" || exit

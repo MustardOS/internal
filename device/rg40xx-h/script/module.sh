5153c6af4ec9a951cd02041092d83063fc8086d8
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-03-22 21:02:38 +1000

    Device specific module loading optimisations

--- device/rg40xx-h/script/module.sh
+++ device/rg40xx-h/script/module.sh
@@ -2,23 +2,30 @@
 
 . /opt/muos/script/var/func.sh
 
-MODULES="mali_kbase squashfs"
-for KMOD in $MODULES; do insmod /lib/modules/"$KMOD".ko || printf "Failed to load %s module\n" "$KMOD"; done
+for KMOD in mali_kbase squashfs; do
+	insmod /lib/modules/"$KMOD".ko || printf "Failed to load %s module\n" "$KMOD"
+done
 
 # Initialise the network module if we have one
 if [ "$(GET_VAR "device" "board/network")" -eq 1 ]; then
-	modprobe --force-modversion "$(GET_VAR "device" "network/module")"
-	while [ ! -d "/sys/class/net/$(GET_VAR "device" "network/iface")" ]; do sleep 0.25; done
+	NET_MODULE=$(GET_VAR "device" "network/module")
+	NET_IFACE=$(GET_VAR "device" "network/iface")
+	DNS_ADDR=$(GET_VAR "global" "network/dns")
+
+	# Wait until the network interface is created
+	modprobe --force-modversion "$NET_MODULE"
+	while [ ! -d "/sys/class/net/$NET_IFACE" ]; do sleep 0.1; done
 
 	rfkill unblock all
 
-	ip link set "$(GET_VAR "device" "network/iface")" up
-	iw dev "$(GET_VAR "device" "network/iface")" set power_save off
+	ip link set "$NET_IFACE" up
+	iw dev "$NET_IFACE" set power_save off
 
-	echo "nameserver $(GET_VAR "global" "network/dns")" >/etc/resolv.conf
+	echo "nameserver $DNS_ADDR" >/etc/resolv.conf
 fi
 
 # Switch GPU power policy and set to maximum frequency
-echo always_on >/sys/devices/platform/gpu/power_policy
-echo 648000000 >/sys/devices/platform/gpu/devfreq/gpu/min_freq
-echo 648000000 >/sys/devices/platform/gpu/devfreq/gpu/max_freq
+GPU_PATH="/sys/devices/platform/gpu"
+echo always_on >"$GPU_PATH/power_policy"
+echo 648000000 >"$GPU_PATH/devfreq/gpu/min_freq"
+echo 648000000 >"$GPU_PATH/devfreq/gpu/max_freq"

a86c76e8d377e4acced5e0f8e518d0acf3122b2a
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-03-22 21:05:42 +1000

    Remove unused audio source

--- script/var/init/system.sh
+++ script/var/init/system.sh
@@ -10,10 +10,9 @@ DEVICE_CONFIG="/opt/muos/device/$DEVICE_TYPE/config.ini"
 DEVICE_CONTROL_DIR="/opt/muos/device/$DEVICE_TYPE/control"
 MUOS_BOOT_LOG="/opt/muos/boot.log"
 ALSA_CONFIG="/usr/share/alsa/alsa.conf"
-AUDIO_SRC="/tmp/mux_audio_src"
 
 export GLOBAL_CONFIG KIOSK_CONFIG DBUS_SESSION_BUS_ADDRESS PIPEWIRE_RUNTIME_DIR XDG_RUNTIME_DIR \
-	DEVICE_TYPE DEVICE_CONFIG DEVICE_CONTROL_DIR MUOS_BOOT_LOG ALSA_CONFIG AUDIO_SRC
+	DEVICE_TYPE DEVICE_CONFIG DEVICE_CONTROL_DIR MUOS_BOOT_LOG ALSA_CONFIG
 
 mkdir -p "/run/muos/system"
 touch "/run/muos/system/foreground_process"

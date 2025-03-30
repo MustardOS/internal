147d7492a711bd53578d2851ef5ce151f9bd307b
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-03-30 21:41:08 +1000

    Updated serial script to revert to hexdump of urandom

--- script/system/serial.sh
+++ script/system/serial.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Prints the current device's unique serial number.
-
-# For H700 devices, this comes from the SID / Chip-ID
+#
+# For most devices, this comes from the SID / Chip-ID
 # (https://linux-sunxi.org/SID_Register_Guide). The stock U-Boot passes the
 # serial number portion of the Chip-ID in the snum kernel command line param.
 #
@@ -12,7 +12,10 @@
 # snum:                  ZZZZ      YYYYYYY XXXXXXXX
 # sunxi_chipid: 33806c00 ZZZZ4808 0YYYYYYY XXXXXXXX
 # sunxi_serial: XXXXXXXX 0YYYYYYY 0000ZZZZ 00000000
+#
+# Otherwise we'll revert back to grabbing a random hexdump value from urandom.
 
 SERIAL="$(xargs -n 1 -a /proc/cmdline | sed -n s/^snum=//p)"
-[ -n "$SERIAL" ] || exit 1
-printf '%s\n' "$SERIAL"
+[ -n "$SERIAL" ] || SERIAL=$(hexdump -vn4 -e'4/4 "%08X" 1 "\n"' /dev/urandom | tr '[:upper:]' '[:lower:]')
+
+printf "%s" "$SERIAL"

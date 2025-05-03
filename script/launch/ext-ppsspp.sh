3d1b4c2c78029bf0c237cf8bb014e28d2718b988
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-05-03 18:25:04 +1000

    Adjusted script for PPSSPP on TrimUI devices

--- script/launch/ext-ppsspp.sh
+++ script/launch/ext-ppsspp.sh
@@ -13,34 +13,45 @@ FILE=${3%/}
 	LOG_INFO "$0" 0 "FILE" "$FILE"
 ) &
 
+PPSSPP_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/ppsspp"
 
 case "$(GET_VAR "device" "board/name")" in
-    rg*) PPSSPP_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/ppsspp/rg"
-        if [ "$(GET_VAR "global" "boot/device_mode")" -eq 1 ]; then
-            SDL_HQ_SCALER=2
-            SDL_ROTATION=0
-            SDL_BLITTER_DISABLED=1
-        else
-            SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
-            SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
-            SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
-        fi
-        export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
+	rg*)
+		PPSSPP_DIR="${PPSSPP_DIR}/rg"
 
-        sed -i '/^GraphicsBackend\|^FailedGraphicsBackends\|^DisabledGraphicsBackends/d' "$PPSSPP_DIR/.config/ppsspp/PSP/SYSTEM/ppsspp.ini" ;;
-    tui*) PPSSPP_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/ppsspp/tui" 
-		ROM_MOUNT="$(GET_VAR "device" "storage/rom/mount")"
-		FILE=$(echo "$FILE" | sed "s|^/mnt/union|$ROM_MOUNT|")
-        echo 1 >/sys/module/pvrsrvkm/parameters/DisableClockGating
-        echo 1 >/sys/module/pvrsrvkm/parameters/EnableFWContextSwitch
-        echo 1 >/sys/module/pvrsrvkm/parameters/EnableSoftResetContextSwitch
-        echo 0 >/sys/module/pvrsrvkm/parameters/PVRDebugLevel
-        export LD_LIBRARY_PATH="$PPSSPP_DIR/lib:$LD_LIBRARY_PATH"
-        rm -rf "$PPSSPP_DIR/.config/ppsspp/PSP/SYSTEM/CACHE/"* ;;
+		if [ "$(GET_VAR "global" "boot/device_mode")" -eq 1 ]; then
+			SDL_HQ_SCALER=2
+			SDL_ROTATION=0
+			SDL_BLITTER_DISABLED=1
+		else
+			SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
+			SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
+			SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
+		fi
+
+		export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
+
+		sed -i '/^GraphicsBackend\|^FailedGraphicsBackends\|^DisabledGraphicsBackends/d' \
+			"$PPSSPP_DIR/.config/ppsspp/PSP/SYSTEM/ppsspp.ini"
+		;;
+	tui*)
+		PPSSPP_DIR="${PPSSPP_DIR}/tui"
+
+		echo 1 >/sys/module/pvrsrvkm/parameters/DisableClockGating
+		echo 1 >/sys/module/pvrsrvkm/parameters/EnableFWContextSwitch
+		echo 1 >/sys/module/pvrsrvkm/parameters/EnableSoftResetContextSwitch
+		echo 0 >/sys/module/pvrsrvkm/parameters/PVRDebugLevel
+
+		export LD_LIBRARY_PATH="$PPSSPP_DIR/lib:$LD_LIBRARY_PATH"
+		rm -rf "$PPSSPP_DIR/.config/ppsspp/PSP/SYSTEM/CACHE/"*
+
+		echo 1000000 >"$(GET_VAR "device" "cpu/min_freq")"
+		;;
 esac
 
 HOME="$PPSSPP_DIR"
 export HOME
+
 XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CONFIG_HOME
 
@@ -52,15 +63,11 @@ case "$FILE" in
 		if [ -e "$PPSSPP_DIR/.config/ppsspp/PSP/GAME/$GAME/EBOOT.PBP" ]; then
 			FILE="$PPSSPP_DIR/.config/ppsspp/PSP/GAME/$GAME/EBOOT.PBP"
 		else
-			GAMESUBDIR=$(find "$GAMEDIR" -maxdepth 2 -type d \( -iname "$GAME" -o -iname ".$GAME" \) )
+			GAMESUBDIR=$(find "$GAMEDIR" -maxdepth 2 -type d \( -iname "$GAME" -o -iname ".$GAME" \))
 			if [ -n "$GAMESUBDIR" ]; then
 				if [ -e "$GAMESUBDIR/EBOOT.PBP" ]; then
 					FILE="$GAMESUBDIR/EBOOT.PBP"
-				else
-					echo >&2 "Game folder $GAMESUBDIR exists, but no EBOOT.PBP found"
 				fi
-			else
-				echo >&2 "Game folder not found for $GAME"
 			fi
 		fi
 		;;

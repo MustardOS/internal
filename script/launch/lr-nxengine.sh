538e606feb72e571659b45c2ae7e9c33f70b7d06
antiKk <4548266+antiKk@users.noreply.github.com>
2025-05-17 18:53:04 +1000

    Updated Cave Story launch script. (#548)
    Co-authored-by: antiKk <antikk@hobbiton.lan>

--- script/launch/lr-nxengine.sh
+++ script/launch/lr-nxengine.sh
@@ -28,35 +28,15 @@ fi
 
 export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
 
-SET_VAR "system" "foreground_process" "retroarch"
-
-MESSAGE() {
-	_TITLE=$1
-	_MESSAGE=$2
-	_FORM=$(
-		cat <<EOF
-$_TITLE
-
-$_MESSAGE
-EOF
-	)
-	/opt/muos/extra/muxstart 0 "$_FORM" && sleep "$3"
-}
-
-F_PATH=$(echo "$FILE" | awk -F'/' '{NF--; print}' OFS='/')
-DOUK="$F_PATH/.Cave Story (En)/Doukutsu.exe"
-
 LOGPATH="$(GET_VAR "device" "storage/rom/mount")/MUOS/log/nxe.log"
 
-RA_CONF=/run/muos/storage/info/config/retroarch.cfg
+SET_VAR "system" "foreground_process" "retroarch"
 
-# Include default button mappings from retroarch.device.cfg. (Settings
-# in the retroarch.cfg will take precedence. Modified settings will save
-# to the main retroarch.cfg, not the included retroarch.device.cfg.)
-sed -n -e '/^#include /!p' \
-	-e '$a#include "/opt/muos/device/current/control/retroarch.device.cfg"' \
-	-e '$a#include "/opt/muos/device/current/control/retroarch.resolution.cfg"' \
-	-i "$RA_CONF"
+echo "Starting Cave Story (libretro)" > "$LOGPATH"
+# Set nxengine BIOS path
+DOUK_BIOS="/run/muos/storage/bios/nxengine/Doukutsu.exe"
+
+RA_CONF=/run/muos/storage/info/config/retroarch.cfg
 
 if [ "$(GET_VAR "kiosk" "content/retroarch")" -eq 1 ] 2>/dev/null; then
 	sed -i 's/^kiosk_mode_enable = "false"$/kiosk_mode_enable = "true"/' "$RA_CONF"
@@ -64,58 +44,59 @@ else
 	sed -i 's/^kiosk_mode_enable = "true"$/kiosk_mode_enable = "false"/' "$RA_CONF"
 fi
 
-if [ -e "$DOUK" ]; then
-	retroarch -v -c "$RA_CONF" -L "$(GET_VAR "device" "storage/rom/mount")/MUOS/core/$CORE" "$DOUK" &
-	RA_PID=$!
+if [ -e "$DOUK_BIOS" ]; then
+	echo "Doukutsu.exe found!" >> "$LOGPATH"
+	GREENLIGHT=1
+
 else
+	echo "Doukutsu.exe NOT found!" >> "$LOGPATH"
 	CZ_NAME="Cave Story (En).zip"
 	CAVE_URL="https://bot.libretro.com/assets/cores/Cave Story/$CZ_NAME"
+	echo "Cave Story URL: $CAVE_URL" >> "$LOGPATH"
 	BIOS_FOLDER="/run/muos/storage/bios/"
 
-	if [ -e "$BIOS_FOLDER$CZ_NAME" ]; then
-		echo "$CZ_NAME exists at $BIOS_FOLDER" >>"$LOGPATH"
-	else
-		echo "$CZ_NAME not found in $BIOS_FOLDER" >>"$LOGPATH"
-		## Is this thing on(line)?
-		check_internet() {
-			echo "Pinging github.com" >>"$LOGPATH"
-			ping -c 1 github.com >/dev/null 2>&1
-			return $?
-		}
-		if check_internet; then
-			echo "Downloading from $CAVE_URL" >>"$LOGPATH"
-			wget -O "$BIOS_FOLDER$CZ_NAME" "$CAVE_URL"
+	echo "$DOUK_BIOS not found in $BIOS_FOLDER"
+	# Is this thing on(line)?
+	check_internet() {
+		echo "Pinging github.com" >> "$LOGPATH"
+		ping -c 1 github.com >/dev/null 2>&1
+		return $?
+	}
+	if check_internet; then
+		# Download
+		echo "Downloading from $CAVE_URL" >>"$LOGPATH"
+		wget -O "$BIOS_FOLDER$CZ_NAME" "$CAVE_URL"
+	
+		# Extract
+		echo "Extracting $CZ_NAME to $BIOS_FOLDER/Cave Story (En)" >> "$LOGPATH"
+		unzip -o "$BIOS_FOLDER$CZ_NAME" -d "$BIOS_FOLDER"
+		if [ -e "$BIOS_FOLDER/Cave Story (En)" ]; then
+			echo "Renaming Cave Story (En) Folder to nxengine" >> "$LOGPATH"
+			mv "$BIOS_FOLDER/Cave Story (En)" "$BIOS_FOLDER/nxengine"
+			# Cleanup
+			echo "Removing $CZ_NAME"
+			rm -f "$BIOS_FOLDER$CZ_NAME"
+			GREENLIGHT=1
+		elif [ -e "$BIOS_FOLDER/nxengine" ]; then
+			echo "Already renamed" >> "$LOGPATH"
+			GREENLIGHT=1
 		else
-			# If local copy doesn't exist and cannot download a copy, pop message
-			echo "Unable to download $CZ_NAME" >>"$LOGPATH"
-			TITLE="Missing File"
-			CONTENT="Cave Story (En).zip not found in /MUOS/bios
-			Please see https://muos.dev for more information!"
-			MESSAGE "$TITLE" "$CONTENT" 5
+			echo "Did extraction fail?" >> "$LOGPATH"
 		fi
-	fi
-
-	## Extract the zip
-	echo "Extracting $CZ_NAME to $F_PATH" >>"$LOGPATH"
-	unzip -o "$BIOS_FOLDER$CZ_NAME" -d "$F_PATH"
-
-	if [ -e "$F_PATH/Cave Story (En)" ]; then
-		echo "Hiding folder" >>"$LOGPATH"
-		mv "$F_PATH/Cave Story (En)" "$F_PATH/.Cave Story (En)"
-	elif [ -e "$F_PATH/.Cave Story (En)" ]; then
-		echo "Already hidden" >>"$LOGPATH"
 	else
-		echo "Did extraction fail?" >>"$LOGPATH"
+		echo "Unable to download $CZ_NAME" >> "$LOGPATH"
+		GREENLIGHT=0
 	fi
-
-	/opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" start
-
-	retroarch -v -c "$RA_CONF" -L "$(GET_VAR "device" "storage/rom/mount")/MUOS/core/$CORE" "$DOUK" &
-	RA_PID=$!
 fi
 
-wait $RA_PID
+if [ "$GREENLIGHT" -eq 1 ]; then
+	echo "Launching Cave Story" >> "$LOGPATH"
+	/opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" start
+	retroarch -v -c "$RA_CONF" -L "$(GET_VAR "device" "storage/rom/mount")/MUOS/core/$CORE" "$DOUK" &
+	RA_PID=$!
 
-/opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" stop
+	wait $RA_PID
+	/opt/muos/script/mux/track.sh "$NAME" "$CORE" "$FILE" stop
+fi
 
 unset SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED

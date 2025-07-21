2df964fdc868c44fc11a6f28372b371828dd1b95
arkun <56165886+CatalyticArkun@users.noreply.github.com>
2025-07-21 04:48:33 -0500

    Add Syncthing auto-scan toggle and add back API calls to launch and quit scripts (#577)

---
+++ init/MUOS/task/Syncthing Tasks/Toggle Syncthing Auto-Scan.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# HELP: Toggle Syncthing Auto-Scan
+# ICON: network
+
+# This script toggles if API calls to Syncthing after content close
+# and shutdown are enabled or not.
+
+. /opt/muos/script/var/func.sh
+
+FRONTEND stop
+
+if [ "$(GET_VAR "config" "syncthing/auto_scan")" -eq 0 ]; then
+	echo "Turning on Syncthing Auto-Scan"
+	SET_VAR "config" "syncthing/auto_scan" "1"
+else
+	echo "Turning off Syncthing Auto-Scan"
+	SET_VAR "config" "syncthing/auto_scan" "0"
+fi
+
+echo "Sync Filesystem"
+sync
+
+echo "All Done!"
+/opt/muos/bin/toybox sleep 2
+
+FRONTEND start task
+exit 0

68d9e7c7d6957b60e79b86c045399f5015ce0d06
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-07-23 19:05:05 +1000

    Wait for interface before MAC change and store MAC value

--- script/web/macchange.sh
+++ script/web/macchange.sh
@@ -2,14 +2,28 @@
 
 . /opt/muos/script/var/func.sh
 
+HAS_NETWORK=$(GET_VAR "device" "board/network")
 IFCE=$(GET_VAR "device" "network/iface")
+
+[ "$HAS_NETWORK" -eq 1 ] && /opt/muos/script/system/network.sh disconnect
 
 case "$(GET_VAR "device" "board/name")" in
 	tui*) /opt/muos/device/script/module.sh load-network ;;
 	*) ;;
 esac
 
+WAIT_IFACE=20
+while [ "$WAIT_IFACE" -gt 0 ]; do
+	[ -d "/sys/class/net/$IFCE" ] && break
+
+	/opt/muos/bin/toybox sleep 1
+	WAIT_IFACE=$((WAIT_IFACE - 1))
+done
+
+ip link set dev "$IFCE" down
 /usr/bin/macchanger -r "$IFCE"
+
+SET_VAR "config" "network/mac" "$NEW_MAC"
 
 case "$(GET_VAR "device" "board/name")" in
 	tui*) /opt/muos/device/script/module.sh unload-network ;;

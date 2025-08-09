d13b6268ef832aaf81d0bd304ecbe5f8155f628a
antiKk <4548266+antiKk@users.noreply.github.com>
2025-08-09 20:26:15 +1000

    Fixed broken screenshots on 28xx-h (#579)
    Co-authored-by: antiKk <antikk@hobbiton.lan>

--- script/mux/screenshot.sh
+++ script/mux/screenshot.sh
@@ -5,27 +5,29 @@
 SS_LOCK="/tmp/screenshot.lock"
 
 if [ ! -e "$SS_LOCK" ]; then
-	RUMBLE_DEVICE="$(GET_VAR "device" "board/rumble")"
-	SCREEN_WIDTH="$(GET_VAR "device" "screen/width")"
-	SCREEN_HEIGHT="$(GET_VAR "device" "screen/height")"
-	SCREEN_ROTATE="$(GET_VAR "device" "screen/rotate")"
+    RUMBLE_DEVICE="$(GET_VAR "device" "board/rumble")"
+    SCREEN_WIDTH="$(GET_VAR "device" "screen/width")"
+    SCREEN_HEIGHT="$(GET_VAR "device" "screen/height")"
 
-	RUMBLE "$RUMBLE_DEVICE" 0.3
+    RUMBLE "$RUMBLE_DEVICE" 0.3
 
-	touch "$SS_LOCK"
+    touch "$SS_LOCK"
 
-	BASE_DIR="/run/muos/storage/screenshot"
-	CURRENT_DATE="$(date +"%Y%m%d_%H%M")"
-	INDEX=0
+    BASE_DIR="/run/muos/storage/screenshot"
+    CURRENT_DATE="$(date +"%Y%m%d_%H%M")"
+    INDEX=0
 
-	while :; do
-		SS_FILE="${BASE_DIR}/muOS_${CURRENT_DATE}_${INDEX}.png"
-		[ ! -f "$SS_FILE" ] && break
-		INDEX=$((INDEX + 1))
-	done
+    while :; do
+        SS_FILE="${BASE_DIR}/muOS_${CURRENT_DATE}_${INDEX}.png"
+        [ ! -f "$SS_FILE" ] && break
+        INDEX=$((INDEX + 1))
+    done
 
-	fbgrab -a -w "$SCREEN_WIDTH" -h "$SCREEN_HEIGHT" -l "$SCREEN_WIDTH" "$SS_FILE"
-	[ "$SCREEN_ROTATE" -eq 1 ] && convert "$SS_FILE" -rotate 90 "$SS_FILE"
+    # Silly 28xx...
+    case "$(GET_VAR "device" "board/name")" in
+        rg28xx-h) fbgrab -a -w "$SCREEN_HEIGHT" -h "$SCREEN_WIDTH" -l "$SCREEN_HEIGHT" "$SS_FILE" && convert "$SS_FILE" -rotate 90 "$SS_FILE" ;;
+        *) fbgrab -a -w "$SCREEN_WIDTH" -h "$SCREEN_HEIGHT" -l "$SCREEN_HEIGHT" "$SS_FILE" ;;
+    esac
 
-	rm "$SS_LOCK"
+    rm "$SS_LOCK"
 fi

4a1aab6b51add74adbcae79674a7883fb2bb9d85
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-08-20 12:19:54 +1000

    Optimised idle inhibit script for current running processes

--- script/mux/idle.sh
+++ script/mux/idle.sh
@@ -8,12 +8,16 @@ INHIBIT_NONE=0
 INHIBIT_BOTH=1
 INHIBIT_SLEEP=2
 
+DO_BRIGHT=10
+KEEP_BRIGHT=
+
 DISPLAY_IDLE() {
 	[ "$(GET_VAR "config" "settings/power/idle_mute")" -eq 1 ] && wpctl set-mute @DEFAULT_AUDIO_SINK@ "1"
 
-	if [ "$(DISPLAY_READ lcd0 getbl)" -gt 10 ]; then
-		DISPLAY_WRITE lcd0 setbl 10
-	fi
+	BL="$(GET_VAR "config" "settings/general/brightness")"
+	KEEP_BRIGHT=$BL
+
+	[ "$BL" -gt "$DO_BRIGHT" ] && /opt/muos/device/script/bright.sh "$DO_BRIGHT"
 
 	[ -f "$LED_CONTROL_SCRIPT" ] && "$LED_CONTROL_SCRIPT" 1 0 0 0 0 0 0 0
 
@@ -21,32 +25,41 @@ DISPLAY_IDLE() {
 }
 
 DISPLAY_ACTIVE() {
-	[ -e "$IS_IDLE" ] && rm -f "$IS_IDLE"
-
 	[ "$(GET_VAR "config" "settings/power/idle_mute")" -eq 1 ] && wpctl set-mute @DEFAULT_AUDIO_SINK@ "0"
 
 	BL="$(GET_VAR "config" "settings/general/brightness")"
 
-	if [ "$(DISPLAY_READ lcd0 getbl)" -ne "$BL" ]; then
-		DISPLAY_WRITE lcd0 setbl "$BL"
-	fi
+	[ "$BL" -ne "$KEEP_BRIGHT" ] && /opt/muos/device/script/bright.sh "$KEEP_BRIGHT"
 
 	LED_CONTROL_CHANGE
+
+	[ -e "$IS_IDLE" ] && rm -f "$IS_IDLE"
 }
 
-# Monitor for specific programs that should inhibit idle timeout.
+# Processes we need to look out for... just make sure to keep the space before and after!
+WATCHLIST=" syncthing ffplay mpv muterm muxcharge muxbackup muxarchive muxcredits muxmessage "
+
 while :; do
 	INHIBIT="$INHIBIT_NONE"
 
-	# Inhibit idle sleep while charging.
-	if [ "$(cat "$(GET_VAR "device" "battery/charger")")" -eq 1 ]; then
-		INHIBIT="$INHIBIT_SLEEP"
+	CHARGER_PATH="$(GET_VAR "device" "battery/charger")"
+	if [ -r "$CHARGER_PATH" ]; then
+		IFS= read -r CHARGING <"$CHARGER_PATH" || CHARGING=0
+		[ "$CHARGING" -eq 1 ] && INHIBIT="$INHIBIT_SLEEP"
 	fi
 
-	# Inhibit idle display and sleep during long-running processes.
-	case "$(GET_VAR "system" "foreground_process")" in
-		muterm | ffplay | mpv | muxcharge | muxcredits | muxmessage | muxbackup) INHIBIT="$INHIBIT_BOTH" ;;
-	esac
+	# Have a peek at all of the running processes and break
+	# if one is matched from our watch list
+	for PROC in /proc/[0-9]*/comm; do
+		[ -r "$PROC" ] || continue
+		IFS= read -r P <"$PROC" || continue
+		case "$WATCHLIST" in
+			*" $P "*)
+				INHIBIT="$INHIBIT_BOTH"
+				break
+				;;
+		esac
+	done
 
 	SET_VAR "system" "idle_inhibit" "$INHIBIT"
 	/opt/muos/bin/toybox sleep 5

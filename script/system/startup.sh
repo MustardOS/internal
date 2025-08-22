be2f06dd5ecab97567729c112e92becd63a83708
Corey Clark <cmclark00@gmail.com>
2025-08-22 01:30:51 -0400

    changing >  / back to >/ from where i changed it previously

--- script/system/startup.sh
+++ script/system/startup.sh
@@ -43,9 +43,9 @@ case "$BOARD_NAME" in
 		echo 0 >/sys/class/gpio/gpio227/value
 		;;
 	rk*)
-		echo 0 > /sys/class/pwm/pwmchip0/export
-		echo 1000000 > /sys/class/pwm/pwmchip0/pwm0/period
-		echo 1000000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+		echo 0 >/sys/class/pwm/pwmchip0/export
+		echo 1000000 >/sys/class/pwm/pwmchip0/pwm0/period
+		echo 1000000 >/sys/class/pwm/pwmchip0/pwm0/duty_cycle
 		echo 1 >/sys/class/pwm/pwmchip0/pwm0/enable
 		;;
 	*) ;;

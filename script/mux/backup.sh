c3b536ccb68135b413554edbc7a194dea36c90e5
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-10 12:09:40 +1000

    Modified backup script to utilise new archive format

--- script/mux/backup.sh
+++ script/mux/backup.sh
@@ -1,42 +1,17 @@
 #!/bin/sh
-# backup.sh - A script to backup files from MUOS devices
-# This script reads a manifest file to determine which files to back up,
+# The following script reads a manifest file to determine which files to back up,
 # where to back them up, and whether to do it in individual or batch mode.
 # It supports both individual backups and batch processing of multiple files.
 
-LOGFILE="/tmp/muxbackup.log"
-exec > >(tee -a "$LOGFILE") 2>&1
-
 . /opt/muos/script/var/func.sh
+. /opt/muos/script/var/zip.sh
+
 FRONTEND stop
 
 SET_VAR "system" "foreground_process" "muxbackup"
 
-# VARIABLES
 MANIFEST_FILE="/tmp/muxbackup_manifest.txt"
 ERROR_FLAG=0
-
-# START SCRIPT
-echo "Starting backup script at $(date +"%Y-%m-%d %H:%M:%S")"
-
-# Check if manifest file exists
-if [ ! -f "$MANIFEST_FILE" ]; then
-	echo "Manifest file not found: $MANIFEST_FILE"
-	ERROR_FLAG=1
-elif ! read -r SRC_MODE DEST_MNT <"$MANIFEST_FILE"; then
-	echo "Failed to read manifest header from $MANIFEST_FILE"
-	ERROR_FLAG=1
-# Read the first line of the manifest file to get SRC_MODE and DEST_MNT
-elif [ -z "$SRC_MODE" ] || [ -z "$DEST_MNT" ]; then
-	echo "Invalid manifest header format. Expected: SRC_MODE DEST_MNT"
-	ERROR_FLAG=1
-elif [ "$SRC_MODE" != "INDIVIDUAL" ] && [ "$SRC_MODE" != "BATCH" ]; then
-	echo "Invalid SRC_MODE in manifest: $SRC_MODE"
-	ERROR_FLAG=1
-elif [ "$DEST_MNT" != "SD1" ] && [ "$DEST_MNT" != "SD2" ] && [ "$DEST_MNT" != "USB" ]; then
-	echo "Invalid DEST_MNT in manifest: $DEST_MNT"
-	ERROR_FLAG=1
-fi
 
 BACKUP_FOLDER="BACKUP"
 
@@ -44,28 +19,37 @@ SD1="$(GET_VAR "device" "storage/rom/mount")"
 SD2="$(GET_VAR "device" "storage/sdcard/mount")"
 USB="$(GET_VAR "device" "storage/usb/mount")"
 
+# Check if manifest file exists
+if [ ! -f "$MANIFEST_FILE" ]; then
+	printf "\nManifest file not found: %s\n" "$MANIFEST_FILE"
+	ERROR_FLAG=1
+elif ! read -r SRC_MODE DEST_MNT <"$MANIFEST_FILE"; then
+	printf "\nFailed to read manifest header from: %s\n" "$MANIFEST_FILE"
+	ERROR_FLAG=1
+elif [ -z "$SRC_MODE" ] || [ -z "$DEST_MNT" ]; then
+	printf "\nInvalid manifest header format. Expected: SRC_MODE DEST_MNT\n"
+	ERROR_FLAG=1
+elif [ "$SRC_MODE" != "INDIVIDUAL" ] && [ "$SRC_MODE" != "BATCH" ]; then
+	printf "\nInvalid SRC_MODE in manifest: %s\n" "$SRC_MODE"
+	ERROR_FLAG=1
+elif [ "$DEST_MNT" != "SD1" ] && [ "$DEST_MNT" != "SD2" ] && [ "$DEST_MNT" != "USB" ]; then
+	printf "\nInvalid DEST_MNT in manifest: %s\n" "$DEST_MNT"
+	ERROR_FLAG=1
+fi
+
 if [ "$ERROR_FLAG" -ne 1 ]; then
 	case "$DEST_MNT" in
 		SD1) DEST_PATH="$SD1/$BACKUP_FOLDER" ;;
 		SD2) DEST_PATH="$SD2/$BACKUP_FOLDER" ;;
 		USB) DEST_PATH="$USB/$BACKUP_FOLDER" ;;
 	esac
-
-	# Check if destination exists
-	if [ ! -d "$DEST_PATH" ]; then
-		echo "Destination path does not exist: $DEST_PATH"
-		ERROR_FLAG=1
-	fi
+	[ ! -d "$DEST_PATH" ] && mkdir -p "$DEST_PATH"
 fi
 
 LINE_NUM=0
 
 if [ "$ERROR_FLAG" -ne 1 ]; then
-	cd /
-	# Read the manifest file line by line
 	while read -r SRC_MNT SRC_SHORTNAME SRC_SUFFIX; do
-		SRC_PATH=""
-
 		LINE_NUM=$((LINE_NUM + 1))
 
 		# Skip header
@@ -75,215 +59,40 @@ if [ "$ERROR_FLAG" -ne 1 ]; then
 			break
 		# Validate line format
 		elif [ -z "$SRC_MNT" ] || [ -z "$SRC_SHORTNAME" ] || [ -z "$SRC_SUFFIX" ]; then
-			echo "Invalid line $LINE_NUM in manifest: $SRC_MNT $SRC_SHORTNAME $SRC_SUFFIX"
+			printf "\nInvalid line %s in manifest: %s %s %s\n" "$LINE_NUM" "$SRC_MNT" "$SRC_SHORTNAME" "$SRC_SUFFIX"
 			ERROR_FLAG=1
 			break
 		fi
 
-		echo "$SRC_SHORTNAME: $SRC_MNT/$SRC_SUFFIX"
-		# Handle special cases
-		if [ "$SRC_SHORTNAME" = "External" ] || [ "$SRC_SHORTNAME" = "MuosConfig" ]; then
-
-			if [ "$SRC_SHORTNAME" = "External" ]; then
-				# Define Dreamcast VMU source
-				if [ -d "$MUOS_STORE_DIR/bios/dc" ]; then
-					if [ -f "$MUOS_STORE_DIR/bios/dc/dc_nvmem.bin" ]; then
-						DREAMCAST_NVMEM="$MUOS_STORE_DIR/bios/dc/dc_nvmem.bin"
-					fi
-					VMU_SAVES=$(ls "$MUOS_STORE_DIR/bios/dc/vmu_save_"* 2>/dev/null)
-					if [ -n "$VMU_SAVES" ]; then
-						DREAMCAST_VMU="$MUOS_STORE_DIR/bios/dc/vmu_save_"*
-					fi
-				fi
-
-				# Define DraStic source directories
-				if [ -d "$MUOS_SHARE_DIR/emulator/drastic" ]; then
-					DRASTIC_SAVE_DIR="$MUOS_SHARE_DIR/emulator/drastic/backup"
-					DRASTIC_SAVESTATE_DIR="$MUOS_SHARE_DIR/emulator/drastic/savestates"
-				else
-					DRASTIC_SAVE_DIR=""
-					DRASTIC_SAVESTATE_DIR=""
-				fi
-
-				# Define additional RA source directories
-				if [ -d "$(GET_VAR "device" "storage/rom/mount")/.config" ]; then
-					PPSSPP_RA_SAVE_DIR="$(GET_VAR "device" "storage/rom/mount")/.config"
-				else
-					PPSSPP_RA_SAVE_DIR=""
-				fi
-
-				if [ -f "$MUOS_SHARE_DIR/emulator/pico8/pico8_64" ]; then
-					PICO8_64="$MUOS_SHARE_DIR/emulator/pico8/pico8_64"
-				else
-					PICO8_64=""
-				fi
-
-				if [ -f "$MUOS_SHARE_DIR/emulator/pico8/pico8_dyn" ]; then
-					PICO8_DYN="$MUOS_SHARE_DIR/emulator/pico8/pico8_dyn"
-				else
-					PICO8_DYN=""
-				fi
-
-				if [ -f "$MUOS_SHARE_DIR/emulator/pico8/pico8.dat" ]; then
-					PICO8_DAT="$MUOS_SHARE_DIR/emulator/pico8/pico8.dat"
-				else
-					PICO8_DAT=""
-				fi
-
-				# Capture external emulator files
-				TO_BACKUP="
-                $PICO8_64
-                $PICO8_DYN
-                $PICO8_DAT
-                $PPSSPP_RA_SAVE_DIR
-                $DRASTIC_SAVE_DIR
-                $DRASTIC_SAVESTATE_DIR
-                $DREAMCAST_NVMEM
-                $DREAMCAST_VMU
-                "
-			elif [ "$SRC_SHORTNAME" = "MuosConfig" ]; then
-				if [ -d "/opt/muos/config" ]; then
-					MUOS_CONFIG_DIR="/opt/muos/config"
-				else
-					MUOS_CONFIG_DIR=""
-				fi
-
-				# Capture muOS configuration files
-				TO_BACKUP="
-                $MUOS_CONFIG_DIR
-                "
-			fi
-
-			SRC_PATHS=$(mktemp)
-
-			for BACKUP in $TO_BACKUP; do
-				if [ -e "$BACKUP" ]; then
-					echo "$BACKUP" >>"$SRC_PATHS"
-					echo "Found: $BACKUP"
-				fi
-			done
-
-			if [ -s "$SRC_PATHS" ]; then
-				SRC_PATH=""
-				while IFS= read -r FILE; do
-					SRC_PATH="$SRC_PATH $FILE"
-				done <"$SRC_PATHS"
-				SRC_PATH=$(echo "$SRC_PATH" | sed 's/^ *//')
-			fi
-
-			rm "$SRC_PATHS"
-
-			if [ -z "$SRC_PATH" ]; then
-				echo "No source files found for $SRC_SHORTNAME"
-				continue
-			fi
-		else
-			# Validate and assign source mount point
-			case "$SRC_MNT" in
-				SD1) SRC_MNT_PATH="$SD1" ;;
-				SD2) SRC_MNT_PATH="$SD2" ;;
-				USB) SRC_MNT_PATH="$USB" ;;
-				*)
-					echo "Invalid SRC_MNT: $SRC_MNT at line $LINE_NUM"
-					ERROR_FLAG=1
-					break
-					;;
-			esac
-
-			SRC_PATH="$SRC_MNT_PATH/$SRC_SUFFIX"
-
-			# Check if source path exists
-			if [ ! -e "$SRC_PATH" ]; then
-				echo "Source path not found: $SRC_PATH"
-				ERROR_FLAG=1
-				break
-			fi
-		fi
+		[ ! -e "$SRC_SUFFIX" ] && printf "\nSource path not found: %s\n" "$SRC_SUFFIX" && ERROR_FLAG=1 && break
 
 		if [ "$ERROR_FLAG" -eq 0 ]; then
-			DEST_AVAIL=$(df -k "$DEST_PATH" | tail -1 | awk '{print $4}')
+			DEST_FILE="${DEST_PATH}/MustardOS-${SRC_SHORTNAME}-$(date +%Y%m%d-%H%M).muxzip"
 
-			if [ "$SRC_SHORTNAME" = "External" ] || [ "$SRC_SHORTNAME" = "MuosConfig" ]; then
-				SRC_SIZE=0
-				for FILE in $SRC_PATH; do
-					if [ -e "$FILE" ]; then
-						FILE_SIZE=$(du -sk "$FILE" | awk '{print $1}')
-						SRC_SIZE=$((SRC_SIZE + FILE_SIZE))
-						echo "Found source: $FILE ($FILE_SIZE KB)"
-					fi
-				done
-			else
-				# Get source path size
-				SRC_SIZE=$(du -sk "$SRC_PATH" | awk '{print $1}')
-			fi
-
-			echo "Total size of source files: $SRC_SIZE KB"
-
-			if [ -z "$SRC_SIZE" ]; then
-				echo "Error: SRC_SIZE is not set for $SRC_SHORTNAME"
+			CREATE_ARCHIVE "$SRC_SHORTNAME" "$DEST_FILE" "$SRC_SUFFIX" "$SRC_SHORTNAME" "$SRC_SUFFIX" || {
+				printf "\nFailed to add %s for %s\n" "$SRC_SUFFIX" "$SRC_SHORTNAME"
 				ERROR_FLAG=1
 				break
-			elif [ -z "$DEST_AVAIL" ]; then
-				echo "Error: DEST_AVAIL is not set for $SRC_SHORTNAME"
-				ERROR_FLAG=1
-				break
-			elif [ "$SRC_SIZE" -gt "$DEST_AVAIL" ]; then
-				echo "Not enough space for $SRC_SHORTNAME ($SRC_SIZE KB needed, $DEST_AVAIL KB available)"
-				ERROR_FLAG=1
-				break
-			fi
+			}
 
-			# Use -ru0 for already compressed packages, -ru9 for directories, -u9 for files
-			if [ "$SRC_SHORTNAME" = "CataloguePkg" ] || [ "$SRC_SHORTNAME" = "ConfigPkg" ]; then
-				ZIP_FLAGS="-ru0"
-			elif [ "$SRC_SHORTNAME" = "External" ] || [ "$SRC_SHORTNAME" = "MuosConfig" ] || [ -d "$SRC_PATH" ]; then
-				ZIP_FLAGS="-ru9"
-			elif [ -f "$SRC_PATH" ]; then
-				ZIP_FLAGS="-u9"
-			else
-				echo "Source path is neither file nor directory: $SRC_PATH"
-				ERROR_FLAG=1
-				break
-			fi
-
-			DEST_FILE="${DEST_PATH}/muOS-${SRC_SHORTNAME}-$(date +%Y%m%d-%H%M).muxzip"
-
-			echo "Creating archive for $SRC_SHORTNAME at $DEST_FILE"
-			if [ "$SRC_SHORTNAME" = "External" ] || [ "$SRC_SHORTNAME" = "MuosConfig" ]; then
-
-				echo "zip $ZIP_FLAGS "\"$DEST_FILE\"" $SRC_PATH"
-				# Use eval to expand the file list for zip in POSIX shell
-				if ! eval zip $ZIP_FLAGS "\"$DEST_FILE\"" $SRC_PATH; then
-					echo "Failed to create archive for external sources"
-					ERROR_FLAG=1
-					break
-				fi
-			elif ! zip $ZIP_FLAGS "$DEST_FILE" "$SRC_PATH"; then
-				echo "Failed to create archive for $SRC_PATH"
-				ERROR_FLAG=1
-				break
-			fi
-			echo "Created archive for $SRC_SHORTNAME at $DEST_FILE"
+			printf "\nCreated archive for %s at %s\n" "$SRC_SHORTNAME" "$DEST_FILE"
 		fi
 	done <"$MANIFEST_FILE"
 fi
 
 if [ "$ERROR_FLAG" -ne 0 ]; then
-	echo "An error occurred during the backup process."
+	printf "\nErrors occurred during the backup process\n\n"
 else
-	echo "Backup completed successfully."
+	printf "\nBackup completed successfully\n\n"
 fi
 
 # Remove the manifest file
-if [ -f "$MANIFEST_FILE" ]; then
-	rm -f "$MANIFEST_FILE"
-fi
+[ -f "$MANIFEST_FILE" ] && rm -f "$MANIFEST_FILE"
 
-echo "Finished backup script at $(date +"%Y-%m-%d %H:%M:%S")"
 echo "Sync Filesystem"
 sync
 
-TBOX sleep 5
+TBOX sleep 3
 FRONTEND start backup
 
 SET_VAR "system" "foreground_process" "muxfrontend"
--- script/var/zip.sh
+++ script/var/zip.sh
@@ -38,8 +38,7 @@ CHECK_SPACE_FOR_DEST() {
 	HAVE="$(BYTES_FREE "$DEST")"
 
 	if [ "$HAVE" -lt "$NEED" ]; then
-		echo "Error: Not enough free space on '$DEST'"
-		echo "Need $NEED bytes, have $HAVE bytes!"
+		printf "\nError: Not enough free space on '%s'\nNeed %s bytes, have %s bytes!\n" "$DEST" "$NEED" "$HAVE"
 		return 1
 	fi
 
@@ -53,7 +52,7 @@ CORRECT_PATH_ARCHIVE() {
 
 SAFE_ARCHIVE() {
 	if unzip -Z1 "$1" | grep -E -q '^/|(^|/)\.\.(/|$)'; then
-		echo "Error: Archive contains unsafe paths (absolute or '..')." # Damn sith!
+		printf "\nError: Archive contains unsafe paths (absolute or '..')\n" # Damn sith!
 		return 1
 	fi
 }
@@ -62,13 +61,29 @@ CAT_GRID_CLEAR() {
 	EXTRA_DIRS="Application Archive Collection Folder Root Task Theme"
 	for CAT_TYPE in $EXTRA_DIRS; do
 		if CORRECT_PATH_ARCHIVE "$1" "run/muos/storage/info/catalogue/${CAT_TYPE}/grid/"; then
-			echo "Clearing existing ${CAT_TYPE} grid images..."
+			printf "\nClearing existing %s grid images...\n" "$CAT_TYPE"
 			rm -rf "$MUOS_STORE_DIR/info/catalogue/${CAT_TYPE}/grid"
 		fi
 	done
 }
 
+CHECK_ARCHIVE() {
+	if unzip -Zl "$1" | awk 'NR>3 && NF>=8 {print $8}' | grep -vq '^Stored$'; then
+		echo ""
+		echo "WARNING: Archive was NOT created with STORE only compression"
+		echo ""
+		echo "Any PNGs are already compressed so deflate will just slow extracts"
+		echo "Please repackage with: zip -ru0"
+		echo ""
+	fi
+}
+
 EXTRACT_ARCHIVE() {
+	# $1 = LABEL        (for display purposes)
+	# $2 = ARCHIVE_PATH (path to .muxzip or whatever)
+	# $3 = DEST_DIR     (path to where we want to extract contents)
+	# $4 = PATTERN      (what we want to specifically extract) (optional)
+
 	LABEL="$1"
 	ARCHIVE_PATH="$2"
 	DEST_DIR="$3"
@@ -95,13 +110,91 @@ EXTRACT_ARCHIVE() {
 	fi
 }
 
-CHECK_ARCHIVE() {
-	if unzip -Zl "$1" | awk 'NR>3 && NF>=8 {print $8}' | grep -vq '^Stored$'; then
-		echo ""
-		echo "WARNING: Archive was NOT created with STORE only compression"
-		echo ""
-		echo "Any PNGs are already compressed so deflate will just slow extracts"
-		echo "Please repackage with: zip -ru0"
-		echo ""
+CREATE_ARCHIVE() {
+	# $1 = LABEL         (for display purposes)
+	# $2 = DEST_FILE     (path to .muxzip or whatever)
+	# $3 = SRC_MNT_PATH  (/run/muos/storage)
+	# $4 = SRC_SHORTNAME (bios, package, name)
+	# $5 = SRC_SUFFIX    (bios, package/catalogue, /run/muos/storage/bios, /opt/muos/share/info/config)
+
+	LABEL="$1"
+	DEST_FILE="$2"
+	SRC_MNT_PATH="$3"
+	SRC_SHORTNAME="$4"
+	SRC_SUFFIX="$5"
+
+	case "$SRC_SUFFIX" in
+		/*) ABS_SRC_PATH="$SRC_SUFFIX" ;;
+		*) ABS_SRC_PATH="$SRC_MNT_PATH/$SRC_SUFFIX" ;;
+	esac
+
+	if [ ! -e "$ABS_SRC_PATH" ]; then
+		printf "\nSource path not found: %s\n" "$ABS_SRC_PATH"
+		return 1
 	fi
+
+	DEST_DIR="$(dirname "$DEST_FILE")"
+	[ -d "$DEST_DIR" ] || mkdir -p "$DEST_DIR" || {
+		printf "\nCannot create destination directory: %s\n" "$DEST_DIR"
+		return 1
+	}
+
+	case "$ABS_SRC_PATH" in
+		"$SRC_MNT_PATH"/*) REL_FROM_ROOT="${ABS_SRC_PATH#"$SRC_MNT_PATH"/}" ;;
+		/*) REL_FROM_ROOT="${ABS_SRC_PATH#/}" ;;
+		*) REL_FROM_ROOT="$ABS_SRC_PATH" ;;
+	esac
+
+	LAST="${REL_FROM_ROOT##*/}"
+
+	if command -v mktemp >/dev/null 2>&1; then
+		TMP_ROOT="$(mktemp -d "${TMPDIR:-/tmp}/zipstage.XXXXXX")" || return 1
+	else
+		TMP_ROOT="${TMPDIR:-/tmp}/zipstage.$$"
+		rm -rf "$TMP_ROOT" 2>/dev/null || :
+		mkdir -p "$TMP_ROOT" || return 1
+	fi
+
+	# Build a minimal tree so 'zip' sees the desired archive paths starting at SRC_SHORTNAME...
+	# 1) REL starts with "$SRC_SHORTNAME/..." will place symlink at that trailing path inside $SRC_SHORTNAME/
+	# 2) LAST == "$SRC_SHORTNAME"             will place symlink directly as $TMP_ROOT/$SRC_SHORTNAME
+	# 3) Otherwise                            will place symlink as $TMP_ROOT/$SRC_SHORTNAME/$LAST
+
+	DIR_TRAIL="${REL_FROM_ROOT#"$SRC_SHORTNAME"/}"
+
+	if [ "$REL_FROM_ROOT" = "$SRC_SHORTNAME" ] || [ "$LAST" = "$SRC_SHORTNAME" ]; then
+		ln -s "$ABS_SRC_PATH" "$TMP_ROOT/$SRC_SHORTNAME" || {
+			rm -rf "$TMP_ROOT"
+			return 1
+		}
+	elif [ "$DIR_TRAIL" != "$REL_FROM_ROOT" ]; then
+		mkdir -p "$TMP_ROOT/$SRC_SHORTNAME/$(dirname -- "$DIR_TRAIL")"
+		ln -s "$ABS_SRC_PATH" "$TMP_ROOT/$SRC_SHORTNAME/$DIR_TRAIL" || {
+			rm -rf "$TMP_ROOT"
+			return 1
+		}
+	else
+		mkdir -p "$TMP_ROOT/$SRC_SHORTNAME"
+		ln -s "$ABS_SRC_PATH" "$TMP_ROOT/$SRC_SHORTNAME/$LAST" || {
+			rm -rf "$TMP_ROOT"
+			return 1
+		}
+	fi
+
+	printf "Creating Archive at: '%s'\n" "$DEST_FILE"
+
+	(
+		cd "$TMP_ROOT" || exit 2
+		zip -ru0 "$DEST_FILE" "$SRC_SHORTNAME"
+	)
+	RC=$?
+
+	rm -rf "$TMP_ROOT"
+
+	if [ $RC -ne 0 ]; then
+		echo "Archive creation failed for: $LABEL"
+		return 1
+	fi
+
+	return 0
 }

8fd9987138ffeb21409d074c307224238ab9ea11
antiKk <4548266+antiKk@users.noreply.github.com>
2025-09-12 23:07:59 +1000

    Update PPSSPP for tui devices (#622)
    * Fix tui-spoon PPSSPP
    
    * Fix controls
    
    * Update brick controls
    
    * Remove playtime infro from spoon ini
    
    * Swap font for wqy-microhei.ttc
    
    ---------
    
    Co-authored-by: antiKk <antikk@hobbiton.lan>

--- script/launch/ext-ppsspp.sh
+++ script/launch/ext-ppsspp.sh
@@ -27,12 +27,6 @@ case "$(GET_VAR "device" "board/name")" in
 	tui*)
 		# Prevent blackscreen due to "an issue with the ordering of the RGBA 8888 or something like that" (acmeplus 2025)
 		setalpha 0
-
-		# Keep this until we build with Vulkan
-		sed -i '/^GraphicsBackend\|^FailedGraphicsBackends\|^DisabledGraphicsBackends/d' \
-			"$PPSSPP_DIR/.config/ppsspp/PSP/SYSTEM/ppsspp.ini"
-
-		rm -f "$PPSSPP_DIR/.config/ppsspp/PSP/SYSTEM/FailedGraphicsBackends.txt"
 		;;
 esac
 

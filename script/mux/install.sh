788b597ede87266f7e9e03a1e528ae6413d2e739
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-13 22:14:00 +1000

    Added script for installer module

---
+++ script/mux/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+. /opt/muos/script/var/func.sh
+
+ACT_GO=/tmp/act_go
+
+LOG_INFO "$0" 0 "FRONTEND" "Starting Frontend Installer"
+
+read -r START_TIME _ </proc/uptime
+SET_VAR "system" "start_time" "$START_TIME"
+
+while :; do
+	[ -s "$ACT_GO" ] && {
+		IFS= read -r ACTION <"$ACT_GO"
+
+		LOG_INFO "$0" 0 "FRONTEND" "$(printf "Loading '%s' Action" "$ACTION")"
+
+		case "$ACTION" in
+			"installer")
+				touch /tmp/pdi_go
+				EXEC_MUX "installer" "muxfrontend"
+				;;
+
+			"install") ;;
+
+			"shutdown")
+				PLAY_SOUND shutdown
+				/opt/muos/script/mux/quit.sh poweroff frontend
+				;;
+
+			*)
+				printf "Unknown Module: %s\n" "$ACTION" >&2
+				EXEC_MUX "$ACTION" "muxfrontend"
+				;;
+		esac
+	}
+
+done

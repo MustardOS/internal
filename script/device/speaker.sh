3250cd6591afdd4efb6e4ae40948f2e55202f5c9
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-14 16:55:15 +1000

    Optimised speaker reversal script

--- script/device/speaker.sh
+++ script/device/speaker.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
+. /opt/muos/script/var/func.sh
+
+CARD="${CARD:-0}"
+
+CHANNEL_SWITCH() {
+	amixer -q -c "$CARD" set 'OutputL Mixer DACL' "$1"
+	amixer -q -c "$CARD" set 'OutputL Mixer DACR' "$2"
+	amixer -q -c "$CARD" set 'OutputR Mixer DACL' "$2"
+	amixer -q -c "$CARD" set 'OutputR Mixer DACR' "$1"
+}
+
 while :; do
 	STATE="$(cat "/sys/class/power_supply/axp2202-battery/spk_state")"
 
 	if [ "$STATE" != "$LAST_STATE" ]; then
 		case "$STATE" in
-			0)
-				# Headphones: Normal channel mapping
-				amixer -c 0 set 'OutputL Mixer DACL' on
-				amixer -c 0 set 'OutputL Mixer DACR' off
-				amixer -c 0 set 'OutputR Mixer DACL' off
-				amixer -c 0 set 'OutputR Mixer DACR' on
-				;;
-			1)
-				# Speakers: Reversed channel mapping
-				amixer -c 0 set 'OutputL Mixer DACL' on
-				amixer -c 0 set 'OutputL Mixer DACR' off
-				amixer -c 0 set 'OutputR Mixer DACL' off
-				amixer -c 0 set 'OutputR Mixer DACR' on
-				;;
+			0) # Headphones: L<-DACL, R<-DACR
+				CHANNEL_SWITCH on off ;;
+			1) # Speakers: reversed L/R (L<-DACR, R<-DACL)
+				CHANNEL_SWITCH off on ;;
 		esac
 	fi
 

deebb8a8f11d1ff0cdf85a9dbdda62e1f415ae30
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-16 12:39:32 +1000

    Fixed LED control space formatting

--- script/var/func.sh
+++ script/var/func.sh
@@ -634,28 +634,28 @@ KERNEL_TUNING() {
 }
 
 LED_CONTROL_CHANGE() {
-(
-    if [ "$(GET_VAR "device" "led/rgb")" -eq 1 ]; then
-        if [ "$(GET_VAR "config" "settings/general/rgb")" -eq 1 ]; then
-            RGBCONF_SCRIPT="$MUOS_STORE_DIR/theme/active/rgb/rgbconf.sh"
-            TIMEOUT=10
-            WAIT=0
+	(
+		if [ "$(GET_VAR "device" "led/rgb")" -eq 1 ]; then
+			if [ "$(GET_VAR "config" "settings/general/rgb")" -eq 1 ]; then
+				RGBCONF_SCRIPT="$MUOS_STORE_DIR/theme/active/rgb/rgbconf.sh"
+				TIMEOUT=10
+				WAIT=0
 
-            while [ ! -f "$RGBCONF_SCRIPT" ] && [ "$WAIT" -lt "$TIMEOUT" ]; do
-                TBOX sleep 1
-                WAIT=$((WAIT + 1))
-            done
+				while [ ! -f "$RGBCONF_SCRIPT" ] && [ "$WAIT" -lt "$TIMEOUT" ]; do
+					TBOX sleep 1
+					WAIT=$((WAIT + 1))
+				done
 
-            if [ -f "$RGBCONF_SCRIPT" ]; then
-                "$RGBCONF_SCRIPT"
-            elif [ -f "$LED_CONTROL_SCRIPT" ]; then
-                "$LED_CONTROL_SCRIPT" 1 0 0 0 0 0 0 0
-            fi
-        else
-            [ -f "$LED_CONTROL_SCRIPT" ] && "$LED_CONTROL_SCRIPT" 1 0 0 0 0 0 0 0
-        fi
-    fi
-) &
+				if [ -f "$RGBCONF_SCRIPT" ]; then
+					"$RGBCONF_SCRIPT"
+				elif [ -f "$LED_CONTROL_SCRIPT" ]; then
+					"$LED_CONTROL_SCRIPT" 1 0 0 0 0 0 0 0
+				fi
+			else
+				[ -f "$LED_CONTROL_SCRIPT" ] && "$LED_CONTROL_SCRIPT" 1 0 0 0 0 0 0 0
+			fi
+		fi
+	) &
 }
 
 UPDATE_BOOTLOGO() {

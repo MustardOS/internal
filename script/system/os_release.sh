7a8156ae6f4700bb6214dfbe9454d6727a9a3c80
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-16 12:35:36 +1000

    Optimised OS release file updating

--- script/system/os_release.sh
+++ script/system/os_release.sh
@@ -2,18 +2,23 @@
 
 OS_RELEASE="/etc/os-release"
 
+OS_NAME="MustardOS"
+OS_ID="muos"
+
+BUILD_ID=$(cat "/opt/muos/config/system/build")
+
 VERSION_LINE=$(cat "/opt/muos/config/system/version")
 VERSION_ID=${VERSION_LINE%%_*}
 VERSION_CODENAME=${VERSION_LINE#*_}
 
 {
-	printf 'NAME=muOS\n'
+	printf 'NAME=%s\n' "$OS_NAME"
 	printf 'VERSION="%s (%s)"\n' "$VERSION_ID" "$VERSION_CODENAME"
-	printf 'ID=muos\n'
+	printf 'ID=%s\n' "$OS_ID"
 	printf 'VERSION_ID=%s\n' "$VERSION_ID"
-	printf 'PRETTY_NAME="muOS %s (%s)"\n' "$VERSION_ID" "$VERSION_CODENAME"
+	printf 'PRETTY_NAME="%s %s (%s)"\n' "$OS_NAME" "$VERSION_ID" "$VERSION_CODENAME"
 	printf 'ANSI_COLOR="1;33"\n'
 	printf 'HOME_URL="https://muos.dev/"\n'
 	printf 'SUPPORT_URL="https://community.muos.dev/"\n'
-	printf 'BUILD_ID=%s\n' "$(cat "/opt/muos/config/system/build")"
+	printf 'BUILD_ID=%s\n' "$BUILD_ID"
 } >"$OS_RELEASE"

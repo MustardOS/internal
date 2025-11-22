e33ac32b3c8aedb985c00391e28c53b1b372fd8a
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-11-22 16:46:51 +1000

    Modified bind path script with safer routine

--- script/mount/bind.sh
+++ script/mount/bind.sh
@@ -4,7 +4,7 @@
 
 PRIORITY_LOCS="application bios init info/track music save theme"
 STANDARD_LOCS="info/catalogue info/name info/collection info/history network screenshot syncthing package/catalogue package/config"
-MOUNT_FAILURE="/tmp/muos_mount_failure"
+MOUNT_FAILURE="/tmp/muos/mount_failure"
 
 SDCARD_MOUNT="$(GET_VAR "device" "storage/sdcard/mount")"
 ROM_MOUNT="$(GET_VAR "device" "storage/rom/mount")"
@@ -12,13 +12,22 @@ ROM_MOUNT="$(GET_VAR "device" "storage/rom/mount")"
 rm -f "$MUOS_STORE_DIR/mounted" "$MOUNT_FAILURE"
 mkdir -p "$MUOS_STORE_DIR"
 
+SAFE_BIND() {
+	SRC="$1"
+	TGT="$2"
+
+	mkdir -p "$TGT"
+	umount "$TGT" 2>/dev/null
+
+	mount -n --bind "$SRC" "$TGT"
+}
+
 MOUNT_STORAGE() {
 	LIST="$1"
 	GROUP="$2"
 
 	for S_LOC in $LIST; do
 		TGT="$MUOS_STORE_DIR/$S_LOC"
-		mkdir -p "$TGT"
 
 		if [ -d "$SDCARD_MOUNT/MUOS/$S_LOC" ]; then
 			SRC="$SDCARD_MOUNT/MUOS/$S_LOC"
@@ -27,58 +36,55 @@ MOUNT_STORAGE() {
 			SRC="$ROM_MOUNT/MUOS/$S_LOC"
 			LOG_INFO "$0" 0 "BIND MOUNT" "$GROUP: $S_LOC from ROM"
 		else
-			echo "$S_LOC" >>"$MOUNT_FAILURE"
 			LOG_INFO "$0" 0 "BIND MOUNT" "$GROUP: $S_LOC not found"
+			echo "$S_LOC" >>"$MOUNT_FAILURE"
 			continue
 		fi
 
-		umount "$TGT"
-
-		if mount -n --bind "$SRC" "$TGT"; then
+		if SAFE_BIND "$SRC" "$TGT"; then
 			LOG_INFO "$0" 0 "BIND MOUNT" "Mounted $SRC -> $TGT"
 		else
 			LOG_INFO "$0" 0 "BIND MOUNT" "FAILED to mount $SRC -> $TGT"
 			echo "$S_LOC" >>"$MOUNT_FAILURE"
-		fi &
+		fi
 	done
-	wait
 }
 
 LOG_INFO "$0" 0 "BIND MOUNT" "Mounting PRIORITY paths"
 MOUNT_STORAGE "$PRIORITY_LOCS" "PRIORITY"
 
-[ -s "$MOUNT_FAILURE" ] && CRITICAL_FAILURE mount
+[ -s "$MOUNT_FAILURE" ] && CRITICAL_FAILURE mount "$(cat "$MOUNT_FAILURE")"
 touch "$MUOS_STORE_DIR/mounted"
 
 LOG_INFO "$0" 0 "BIND MOUNT" "Mounting STANDARD paths"
 MOUNT_STORAGE "$STANDARD_LOCS" "STANDARD"
 
-[ -s "$MOUNT_FAILURE" ] && CRITICAL_FAILURE mount
+[ -s "$MOUNT_FAILURE" ] && CRITICAL_FAILURE mount "$(cat "$MOUNT_FAILURE")"
 
 # Bind hardcoded paths on SD1's ROM partition (where we can't use symlinks) to subdirs
 # of the appropriate locations under /run/muos/storage ($MUOS_STORE_DIR) bound above.
 BIND_EMULATOR() {
 	TARGET="$MUOS_STORE_DIR/$1"
 	MOUNT="$MUOS_SHARE_DIR/emulator/$2"
+
 	mkdir -p "$TARGET" "$MOUNT"
+	umount "$MOUNT" 2>/dev/null
 
-	umount "$MOUNT"
-
-	if mount -n --bind "$TARGET" "$MOUNT"; then
+	if SAFE_BIND "$TARGET" "$MOUNT"; then
 		LOG_INFO "$0" 0 "BIND MOUNT" "Bound emulator path $TARGET -> $MOUNT"
 	else
 		LOG_INFO "$0" 0 "BIND MOUNT" "FAILED to bind emulator path $TARGET -> $MOUNT"
-		CRITICAL_FAILURE mount
+		CRITICAL_FAILURE mount "$MOUNT"
 	fi
 }
 
 LOG_INFO "$0" 0 "BIND MOUNT" "Binding emulator-specific paths"
 
-BIND_EMULATOR "save/drastic-legacy/backup" "drastic-legacy/backup" &
-BIND_EMULATOR "save/drastic-legacy/savestates" "drastic-legacy/savestates" &
-BIND_EMULATOR "save/file/OpenBOR-Ext" "openbor/userdata/saves/openbor" &
-BIND_EMULATOR "screenshot" "openbor/userdata/screenshots/openbor" &
-BIND_EMULATOR "save/pico8" "pico8/.lexaloffle/pico-8" &
-BIND_EMULATOR "save/game/PPSSPP-Ext" "ppsspp/.config/ppsspp/PSP/GAME" &
-BIND_EMULATOR "save/file/PPSSPP-Ext" "ppsspp/.config/ppsspp/PSP/SAVEDATA" &
-BIND_EMULATOR "save/state/PPSSPP-Ext" "ppsspp/.config/ppsspp/PSP/PPSSPP_STATE" &
+BIND_EMULATOR "save/drastic-legacy/backup" "drastic-legacy/backup"
+BIND_EMULATOR "save/drastic-legacy/savestates" "drastic-legacy/savestates"
+BIND_EMULATOR "save/file/OpenBOR-Ext" "openbor/userdata/saves/openbor"
+BIND_EMULATOR "screenshot" "openbor/userdata/screenshots/openbor"
+BIND_EMULATOR "save/pico8" "pico8/.lexaloffle/pico-8"
+BIND_EMULATOR "save/game/PPSSPP-Ext" "ppsspp/.config/ppsspp/PSP/GAME"
+BIND_EMULATOR "save/file/PPSSPP-Ext" "ppsspp/.config/ppsspp/PSP/SAVEDATA"
+BIND_EMULATOR "save/state/PPSSPP-Ext" "ppsspp/.config/ppsspp/PSP/PPSSPP_STATE"

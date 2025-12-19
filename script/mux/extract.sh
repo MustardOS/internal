4cc73f74009c2ab135bf41a4ac016bf7f2989914
ImCoKeMaN <25371653+ImCoKeMaN@users.noreply.github.com>
2025-12-19 03:18:56 -0500

    Add Splore app when extracting pico-8 archive (#671)
    * Add Splore application directory and launch script
    
    Create directory for Splore application and copy launch script.
    
    * Add files via upload
    
    * Update paths for Splore application directory
    
    * Fix quoting in extract.sh for file copy and permissions
    
    * Update launch command with variable expansion

--- script/mux/extract.sh
+++ script/mux/extract.sh
@@ -50,6 +50,9 @@ case "$ARCHIVE_NAME" in
 
 			if unzip -o -j "$ARCHIVE" "pico-8/*" -d "${BIOS_DIR}/pico-8/"; then
 				printf "Extracted 'pico-8' Folder to '%s'\n" "$BIOS_DIR"
+				mkdir "$MUOS_SHARE_DIR/application/Splore"
+				cp "$MUOS_SHARE_DIR/emulator/pico8/splore.txt" "$MUOS_SHARE_DIR/application/Splore/mux_launch.sh"
+				chmod +x "$MUOS_SHARE_DIR/application/Splore/mux_launch.sh"
 			else
 				printf "Failed to Extract 'pico-8' Folder\n"
 				ALL_DONE 1

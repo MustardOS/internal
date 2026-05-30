afd0b00676405433a8cc7d3d27d0a0a32d8826b4
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-05-30 14:14:16 +1000

    Added proper SSID string escapes

--- script/web/password.sh
+++ script/web/password.sh
@@ -6,13 +6,12 @@ SSID="$(GET_VAR "config" "network/ssid")"
 PASS="$(GET_VAR "config" "network/pass")"
 HIDDEN="$(GET_VAR "config" "network/hidden")"
 
+SSID_WPA="$(GET_VAR "config" "network/ssid_wpa")"
+[ -z "$SSID_WPA" ] && SSID_WPA=$(printf '%s' "$SSID" | sed 's/\\/\\\\/g; s/"/\\"/g')
+
 case ${#PASS} in
-	64)
-		printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tpsk=%s\n}" "$SSID" "$HIDDEN" "$PASS" >"$WPA_CONFIG"
-		;;
-	0)
-		printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tkey_mgmt=NONE\n}" "$SSID" "$HIDDEN" >"$WPA_CONFIG"
-		;;
+	64) printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tpsk=%s\n}" "$SSID_WPA" "$HIDDEN" "$PASS" >"$WPA_CONFIG" ;;
+	0) printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tkey_mgmt=NONE\n}" "$SSID_WPA" "$HIDDEN" >"$WPA_CONFIG" ;;
 	*)
 		WPA_TMP=$(mktemp)
 

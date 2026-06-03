f072d57d89d969ea6c37c988c0dfcc80e8eaf357
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-06-03 21:38:33 +1000

    Added SSID scanning to all network configurations

--- script/web/password.sh
+++ script/web/password.sh
@@ -4,14 +4,13 @@
 
 SSID="$(GET_VAR "config" "network/ssid")"
 PASS="$(GET_VAR "config" "network/pass")"
-HIDDEN="$(GET_VAR "config" "network/hidden")"
 
 SSID_WPA="$(GET_VAR "config" "network/ssid_wpa")"
 [ -z "$SSID_WPA" ] && SSID_WPA=$(printf '%s' "$SSID" | sed 's/\\/\\\\/g; s/"/\\"/g')
 
 case ${#PASS} in
-	64) printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tpsk=%s\n}" "$SSID_WPA" "$HIDDEN" "$PASS" >"$WPA_CONFIG" ;;
-	0) printf "network={\n\tssid=\"%s\"\n\tscan_ssid=%s\n\tkey_mgmt=NONE\n}" "$SSID_WPA" "$HIDDEN" >"$WPA_CONFIG" ;;
+	64) printf "network={\n\tssid=\"%s\"\n\tscan_ssid=1\n\tpsk=%s\n}" "$SSID_WPA" "$PASS" >"$WPA_CONFIG" ;;
+	0) printf "network={\n\tssid=\"%s\"\n\tscan_ssid=1\n\tkey_mgmt=NONE\n}" "$SSID_WPA" >"$WPA_CONFIG" ;;
 	*)
 		WPA_TMP=$(mktemp)
 
@@ -20,7 +19,9 @@ case ${#PASS} in
 
 		# Extract the derived hex PSK and persist it (replacing the plain password)
 		sed -n '/^[[:space:]]*psk=/s/^[[:space:]]*psk=//p' "$WPA_TMP" >"/opt/muos/config/network/pass"
-		sed '/^[[:space:]]*#psk=/d' "$WPA_TMP" >"$WPA_CONFIG"
+
+		# Strip the plaintext psk and inject scan_ssid=1 after the ssid line
+		awk '/ssid=/ { print; print "\tscan_ssid=1"; next } !/^[[:space:]]*#psk=/' "$WPA_TMP" >"$WPA_CONFIG"
 
 		rm -f "$WPA_TMP"
 		;;

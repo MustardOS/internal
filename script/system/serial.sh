377e75385d7f74c7b3add0138fb823752156b26a
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-08-09 20:35:51 +1000

    Modified serial number script with better fallbacks

--- script/system/serial.sh
+++ script/system/serial.sh
@@ -1,22 +1,39 @@
 #!/bin/sh
 
-# Prints the current device's unique serial number.
-#
-# For most devices, this comes from the SID / Chip-ID
-# (https://linux-sunxi.org/SID_Register_Guide). The stock U-Boot passes the
-# serial number portion of the Chip-ID in the snum kernel command line param.
-#
-# On stock kernels, this is also exposed via the sunxi_chipid and sunxi_serial
-# fields in /sys/class/sunxi_info/sys_info. The values line up as follows:
-#
-# snum:                  ZZZZ      YYYYYYY XXXXXXXX
-# sunxi_chipid: 33806c00 ZZZZ4808 0YYYYYYY XXXXXXXX
-# sunxi_serial: XXXXXXXX 0YYYYYYY 0000ZZZZ 00000000
-#
-# Otherwise we'll revert back to grabbing a random hexdump value from urandom.
-
 SERIAL="$(xargs -n 1 -a /proc/cmdline | sed -n s/^snum=//p)"
-[ -n "$SERIAL" ] || SERIAL=$(hexdump -vn4 -e'4/4 "%08X" 1 "\n"' /dev/urandom | tr '[:upper:]' '[:lower:]')
 
-SERIAL=$(echo "$SERIAL" | sed 's/[[:space:]]//g')
+if [ -z "$SERIAL" ] && [ -r /sys/firmware/devicetree/base/serial-number ]; then
+	SERIAL=$(tr -d '\000[:space:]' </sys/firmware/devicetree/base/serial-number)
+fi
+
+if [ -z "$SERIAL" ] && [ -r /var/lib/dbus/machine-id ]; then
+	IFS= read -r SERIAL </var/lib/dbus/machine-id
+fi
+
+if [ -z "$SERIAL" ] && [ -r /etc/machine-id ]; then
+	IFS= read -r SERIAL </etc/machine-id
+fi
+
+if [ -z "$SERIAL" ] && [ -r /opt/muos/config/system/uuid ]; then
+	IFS= read -r SERIAL </opt/muos/config/system/uuid
+fi
+
+SERIAL_FILE=/opt/muos/config/system/serial
+if [ -z "$SERIAL" ] && [ -r "$SERIAL_FILE" ]; then
+	IFS= read -r SERIAL <"$SERIAL_FILE"
+fi
+
+if [ -z "$SERIAL" ]; then
+	SERIAL=$(hexdump -vn4 -e'4/4 "%08X" 1 "\n"' /dev/urandom | tr '[:upper:]' '[:lower:]')
+	mkdir -p "${SERIAL_FILE%/*}"
+	TEMP_FILE="${SERIAL_FILE}.tmp.$$"
+	if printf "%s" "$SERIAL" >"$TEMP_FILE"; then
+		chmod 600 "$TEMP_FILE"
+		mv -f "$TEMP_FILE" "$SERIAL_FILE"
+	else
+		rm -f "$TEMP_FILE"
+	fi
+fi
+
+SERIAL=$(printf "%s" "$SERIAL" | tr -d '[:space:]')
 printf "%s" "$SERIAL"

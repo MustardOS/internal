c758fd521ae553589d868de6c406ffd449ce2acd
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-08-28 22:54:02 +1000

    Updated module script for input modifications

--- script/device/module.sh
+++ script/device/module.sh
@@ -17,6 +17,12 @@ case "$1" in
 		[ "$HAS_NETWORK" -eq 1 ] && modprobe -q "$NET_NAME"
 
 		case "$BOARD_NAME" in
+			gcs-h36s)
+				modprobe -q dc_sunxi
+				if ! pidof muinput >/dev/null 2>&1; then
+					/opt/muos/frontend/muinput &
+				fi
+				;;
 			rg*)
 				modprobe -q mali_kbase
 				modprobe -q squashfs
@@ -27,18 +33,54 @@ case "$1" in
 					printf "648000000" >"$GPU_PATH/devfreq/gpu/min_freq"
 					printf "648000000" >"$GPU_PATH/devfreq/gpu/max_freq"
 				fi
+
+				case "$BOARD_NAME" in
+					rg-vita-pro|rg28xx-h|rg34xx-h|rg34xx-sp|rg35xx-2024|rg35xx-h|rg35xx-plus|rg35xx-pro|rg35xx-sp|rg40xx-h|rg40xx-v|rgcubexx-h|rgsp)
+					if ! pidof muinput >/dev/null 2>&1; then
+						/opt/muos/frontend/muinput &
+					fi
+
+					(
+						DIRECT_KEYS_DRIVER="/sys/bus/platform/drivers/dierct-keys-polled"
+						H700_ISOLATION_ATTEMPT=0
+						while [ "$H700_ISOLATION_ATTEMPT" -lt 50 ]; do
+							if [ -c /dev/muinput/h700-source ]; then
+								rm -f /dev/input/by-path/platform-dierct-keys-polled-event
+								if [ -e "$DIRECT_KEYS_DRIVER/dierct-keys-polled" ]; then
+									printf '%s' "dierct-keys-polled" >"$DIRECT_KEYS_DRIVER/unbind"
+									exit 0
+								fi
+							fi
+							H700_ISOLATION_ATTEMPT=$((H700_ISOLATION_ATTEMPT + 1))
+							sleep 0.1
+						done
+					) &
+					;;
+				esac
 				;;
 			tui*)
 				modprobe -q dc_sunxi
 
-				# Check if any trimui_inputd process is already running
-				# because this script is typically run with suspend too
-				pgrep -f 'trimui_inputd' >/dev/null 2>&1 || /opt/muos/bin/trimui_inputd &
+				case "$BOARD_NAME" in
+					tui-spoon|tui-brick|tui-brick-pro|tui-smpro-s)
+						if ! pidof muinput >/dev/null 2>&1; then
+							/opt/muos/frontend/muinput &
+						fi
+						;;
+				esac
 				;;
 			mgx*)
 				modprobe -q fuse
 				modprobe -q simplepad
 				modprobe -q dc_sunxi
+				if ! pidof muinput >/dev/null 2>&1; then
+					/opt/muos/frontend/muinput &
+				fi
+				;;
+			rk*)
+				if ! pidof muinput >/dev/null 2>&1; then
+					/opt/muos/frontend/muinput &
+				fi
 				;;
 		esac
 		;;
@@ -46,11 +88,15 @@ case "$1" in
 		[ "$HAS_NETWORK" -eq 1 ] && modprobe -qr "$NET_NAME"
 
 		case "$BOARD_NAME" in
+			gcs-h36s) modprobe -qr dc_sunxi ;;
 			rg*)
 				modprobe -qr mali_kbase
 				modprobe -qr squashfs
 				;;
-			mgx*) modprobe -qr simplepad ;;
+			mgx*)
+				modprobe -qr simplepad
+				modprobe -qr dc_sunxi
+				;;
 		esac
 		;;
 	*)

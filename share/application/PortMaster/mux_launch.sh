725e470a6f1fa870fdff6a908ba05bb3fe12f204
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-09-24 20:56:38 +1000

    Added SDL variables to PortMaster launch script to resolve cranky devices

--- share/application/PortMaster/mux_launch.sh
+++ share/application/PortMaster/mux_launch.sh
@@ -13,4 +13,7 @@ SETUP_APP "$APP_BIN" ""
 PORTMASTER_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/PortMaster"
 cd "$PORTMASTER_DIR" || exit
 
+export SDL_RENDER_VSYNC=1
+export SDL_HINT_VIDEO_DOUBLE_BUFFER=1
+
 bash ./PortMaster.sh
